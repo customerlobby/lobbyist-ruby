a526b69ec27feed9be223c9887bfb427c52fea86
David Lains <dlains@gmail.com>
2013-05-23 13:26:37 -0700

    Added the email invitation model to the library for future use.

---
+++ lib/lobbyist/email_invitation.rb
@@ -0,0 +1,51 @@
+module Lobbyist
+  class EmailInvitation < Lobbyist::Base
+    
+    attr_accessor :id, :contact_id, :status, :attempts, :max_attempts, :created_at, :updated_at
+
+    def company
+      @company
+    end
+    
+    def company=(attributes)
+      @company = Company.new(attributes)
+    end
+
+    def contact
+      @contact
+    end
+    
+    def contact=(attributes)
+      @contact = Contact.new(attributes)
+    end
+    
+    def review
+      @review
+    end
+    
+    def review=(attributes)
+      @review = Review.new(attributes)
+    end
+    
+    def self.list(params = {})
+      create_from_response(get('/v1/email_invitations.json', params))
+    end
+    
+    def self.find(id)
+      create_from_response(get("/v1/email_invitations/#{id}.json"))
+    end
+    
+    def self.create(params = {})
+      create_from_response(post("/v1/email_invitations.json", {'email_invitation' => params}))
+    end
+    
+    def self.update(id, params = {})
+      create_from_response(put("/v1/email_invitations/#{id}.json", {'email_invitation' => params}))
+    end
+    
+    # def self.destroy(id)
+    #   create_from_response(delete("/v1/email_invitations/#{id}.json"))
+    # end
+    
+  end
+end
