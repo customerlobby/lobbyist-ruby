8fbade35d7963f2e5e784414d0a01aea8bf07068
spiritbear <indap.shiv@gmail.com>
2013-06-12 11:02:29 -0700

    [Addresses #51532503] Adding wants_emails to the client ruby library

--- lib/lobbyist/contact.rb
+++ lib/lobbyist/contact.rb
@@ -4,12 +4,11 @@ module Lobbyist
     attr_accessor :id, :contact_id, :company_id, :first_name, :last_name,
       :phone_daytime, :phone_alt, :email, :address1, :address2,
       :city, :state, :zip, :country, :last_service_date, :notes,
-      :email_disturb_status, :unsubscribe_reason, :unsubscribed_at,
+      :email_disturb_status, :wants_emails, :unsubscribe_reason, :unsubscribed_at,
       :exclude_referral_marketing, :exclude_retention_marketing,
       :facebook_like, :facebook_login_review, :twitter_follow,
       :date_added, :auto_review_id, :auto_invite_id, :auto_customer_call_id,
       :auto_review_draft_id, :facebook_uid, :facebook_access_token,
       :facebook_session_key
-
   end
 end
