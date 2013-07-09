64ddeac978a9d6743961111cddd5ffced482fb28
Joshua Volz <jdvolz@gmail.com>
2013-07-09 13:45:40 -0700

    [Addresses #53031375] Added key, accessed_at to email_invitation
    Why: these fields were added to the model corresponding to this object.

--- lib/lobbyist/email_invitation.rb
+++ lib/lobbyist/email_invitation.rb
@@ -1,7 +1,7 @@
 module Lobbyist
   class EmailInvitation < Lobbyist::Base
     
-    attr_accessor :id, :contact_id, :status, :attempts, :max_attempts, :created_at, :updated_at
+    attr_accessor :id, :contact_id, :status, :attempts, :max_attempts, :created_at, :updated_at, :key, :accessed_at
 
     def company
       @company
@@ -43,6 +43,10 @@ module Lobbyist
       create_from_response(put("/v1/email_invitations/#{id}.json", {'email_invitation' => params}))
     end
     
+    def self.find_by_email_key(email_key, params={})
+      create_from_response(get("/v1/email_invitations/find_by_email_key/#{email_key}.json"))
+    end
+
     # def self.destroy(id)
     #   create_from_response(delete("/v1/email_invitations/#{id}.json"))
     # end
