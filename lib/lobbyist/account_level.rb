cd0ad50f3a2e38a59e34c431309264fd47ed3c33
sreevs <sreevidya.allada@gmail.com>
2013-07-09 14:25:35 -0700

    [Addressess #53087429] account level - default promos  list on add promo code page , Lobbyist-ruby part  account_levels, validations and refactor

--- lib/lobbyist/account_level.rb
+++ lib/lobbyist/account_level.rb
@@ -3,7 +3,14 @@ module Lobbyist
     
     attr_accessor :account_class_id, :account_class, :description, :call_credit_cost, :default_promo_id, :enabled, :monthly_call_credit_allowance,
     :ccall_credits_accumulate, :monthly_handwritten_credit_allowance, :handwritten_credits_accumulate, :conversion_handwritten_credit_grant, :conversion_ccall_credit_grant
-        
+      
+    def default_promo
+      @default_promo
+    end
+    
+    def default_promo=(attributes)
+      @default_promo = Promo.new(attributes)
+    end   
     def self.list(params = {})
       create_from_response(get('/v1/account_levels.json', params))
     end
