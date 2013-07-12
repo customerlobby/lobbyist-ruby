3ab0f68c3ed968aadc869e35f217648290c12c08
David Lains <dlains@gmail.com>
2013-07-12 10:20:43 -0700

    [addresses #53194035] Fixing problem with ClientError initialization.

--- lib/lobbyist/error/client_error.rb
+++ lib/lobbyist/error/client_error.rb
@@ -4,10 +4,10 @@ module Lobbyist
     # Raised when Customer Lobby returns a 4xx HTTP status code or there's an error in Faraday
     class ClientError < Lobbyist::Error
 
-      def initialize(response, client_message)
+      def initialize(response, client_message = nil)
         data = MultiJson.load(response)
         super(data['errors'])
-        errors << client_message
+        errors << client_message unless client_message == nil
       end
       
     end
