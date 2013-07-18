8783f8d1dcb662430ffb45540eef8486acca6c0f
David Lains <dlains@gmail.com>
2013-07-18 12:20:13 -0700

    [addresses #53097159] Added CustomerCall proxy model to the library.

---
+++ lib/lobbyist/customer_call.rb
@@ -0,0 +1,33 @@
+module Lobbyist
+  class CustomerCall < Lobbyist::Base
+    
+    attr_accessor :id, :company_id, :contact_id, :customer_call_credit_id, :admin_user_id,
+      :status, :attempts, :notes, :last_call_date, :next_call_date, :completed_at, :created_at, :updated_at
+    
+    def self.list(params = {})
+      create_from_response(get('/v1/customer_calls.json', params))
+    end
+    
+    def self.find(id)
+      create_from_response(get("/v1/customer_calls/#{id}.json"))
+    end
+    
+    def self.create(params = {})
+      create_from_response(put("/v1/customer_calls.json", {'customer_call' => params}))
+    end
+    
+    def self.update(id, params = {})
+      create_from_response(put("/v1/customer_calls/#{id}.json", {'customer_call' => params}))
+    end
+    
+    def self.enqueue(id, admin_user_id)
+      params = {'admin_user_id' => admin_user_id}
+      create_from_response(put("/v1/customer_calls/#{id}/enqueue.json", params))
+    end
+
+    def self.dequeue(id)
+      create_from_response(put("/v1/customer_calls/#{id}/dequeue.json"))
+    end
+
+  end
+end
