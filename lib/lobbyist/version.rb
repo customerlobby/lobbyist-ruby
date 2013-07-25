57400d4091cd3163cccb2f7edde17973da3ce683
sreevs <sreevidya.allada@gmail.com>
2013-07-25 09:40:14 -0700

    [addressess #53526071] xref_suggestions model ReviewSuggestion added to lobbyist-ruby

--- lib/lobbyist/version.rb
+++ lib/lobbyist/version.rb
@@ -2,7 +2,7 @@ module Lobbyist
   class Version
     MAJOR = 0 unless defined? Lobbyist::Version::MAJOR
     MINOR = 3 unless defined? Lobbyist::Version::MINOR
-    PATCH = 32 unless defined? Lobbyist::Version::PATCH
+    PATCH = 33 unless defined? Lobbyist::Version::PATCH
 
     class << self
 
