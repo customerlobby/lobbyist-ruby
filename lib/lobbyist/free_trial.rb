dd2c304b5413389a628b8b0825e12e07536dc721
sreevs <sreevidya.allada@gmail.com>
2013-10-07 13:24:29 -0700

    [addressess #57101534] lobbyist-ruby find and update

--- lib/lobbyist/free_trial.rb
+++ lib/lobbyist/free_trial.rb
@@ -5,5 +5,12 @@ module Lobbyist
       create_collection_from_response(get('/v1/free_trials.json', params))
     end
 
+     def self.find(id)
+      create_from_response(get("/v1/free_trials/#{id}.json"))
+    end
+    
+   	def self.update(id, params = {})
+      create_from_response(put("/v1/free_trials/#{id}.json", {'free_trial' => params}))
+    end
   end
 end
