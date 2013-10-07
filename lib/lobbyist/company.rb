d50357f9dff5d730f8f3195fc316fff71d8be9f9
David Lains <dlains@gmail.com>
2013-10-07 10:20:02 -0700

    [addresses #57956686] Removed syndicate column. Company no longer has a syndicate column. Removing the reference from the lobbyist library.

--- lib/lobbyist/company.rb
+++ lib/lobbyist/company.rb
@@ -6,7 +6,7 @@ module Lobbyist
       :website,:is_active, :abbreviated_name, :creation_date, :signup_admin_id, :signup_ip_addr,
       :account_terminated, :termination_date, :activation_code, :account_class, :promo_id, :average_changed,
       :average_score, :admin_notes, :customer_call_notes, :trial_source, :partner_id, :partner_account_id,
-      :syndicate, :last_synd_stat_date, :last_credit_grant, :send_transaction_receipts, :send_transaction_receipts_to,
+      :last_synd_stat_date, :last_credit_grant, :send_transaction_receipts, :send_transaction_receipts_to,
       :sales_status, :date_live, :country, :user_notes, :smart_invite, :enable_referral_marketing, :enable_retention_marketing,
       :rss_queue_push, :rss_queue_attempts, :rss_change_date, :rss_push_date, :feedburner_url, :last_handwritten_review_credit_grant,
       :api_user_name, :api_key_hash, :sugar_lead_id, :sugar_account_id, :sugar_opportunity_id, :company_info_changed,
