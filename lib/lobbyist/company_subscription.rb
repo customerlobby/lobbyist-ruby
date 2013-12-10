c3c11e633d73e3e84ad55a2d990fac97e9ef9760
David Lains <dlains@gmail.com>
2013-12-10 12:11:15 -0800

    [addresses #61583376] Additional methods for company subscription.
    Needed to add the index, show and update api calls to the company subscription proxy model.

--- lib/lobbyist/company_subscription.rb
+++ lib/lobbyist/company_subscription.rb
@@ -1,13 +1,21 @@
 module Lobbyist
   class CompanySubscription < Lobbyist::Base
-    attr_accessor :id, :company_id, :subscription_id, :reference, :authorization_code,
-    attr_accessor :status, :on_hold_since, :last_four, :expiration_date, :card_type,
+    attr_accessor :id, :company_id, :subscription_id, :reference, :authorization_code
+    attr_accessor :status, :on_hold_since, :last_four, :expiration_date, :card_type
     attr_accessor :last_contacted_at, :created_at, :updated_at
     
     def self.list(params = {})
       create_collection_from_response(get("/v1/company_subscriptions.json", params))
     end
 
+    def self.find(id, params = {})
+      create_from_response(get("/v1/company_subscriptions/#{id}.json", params))
+    end
+    
+    def self.update(id, params = {})
+      create_from_response(put("/v1/company_subscriptions/#{id}.json", company_subscription: params))
+    end
+    
     def self.find_by_company(id)
       create_from_response(get("/v1/company_subscriptions/company/#{id}.json"))
     end
