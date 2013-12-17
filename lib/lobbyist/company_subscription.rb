fb2b754fb40dce349072695162292c28ec298e1f
spiritbear <indap.shiv@gmail.com>
2013-12-17 13:43:23 -0800

    [Addresses #62669844] Adding a new method to find subscriptions by company_id, adding the next payment date in the UI

--- lib/lobbyist/company_subscription.rb
+++ lib/lobbyist/company_subscription.rb
@@ -2,7 +2,7 @@ module Lobbyist
   class CompanySubscription < Lobbyist::Base
     attr_accessor :id, :company_id, :subscription_id, :reference, :authorization_code
     attr_accessor :status, :on_hold_since, :last_four, :expiration_date, :card_type
-    attr_accessor :last_contacted_at, :created_at, :updated_at
+    attr_accessor :last_contacted_at, :next_payment_date, :created_at, :updated_at
     
     def self.list(params = {})
       create_collection_from_response(get("/v1/company_subscriptions.json", params))
@@ -14,6 +14,10 @@ module Lobbyist
     
     def self.update(id, params = {})
       create_from_response(put("/v1/company_subscriptions/#{id}.json", company_subscription: params))
+    end
+    
+    def self.find_by_company_id(id)
+      create_from_response(get("/v1/companies/#{id}/subscription.json"))
     end
   end
 end
