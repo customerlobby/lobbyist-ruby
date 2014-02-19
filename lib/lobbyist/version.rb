c59cba97537e3d038a5c80b9fffec7810b79fdec
David Lains <dlains@gmail.com>
2014-02-19 11:02:56 -0800

    Update lobbyist version for release r76.

--- lib/lobbyist/version.rb
+++ lib/lobbyist/version.rb
@@ -2,7 +2,7 @@ module Lobbyist
   class Version
     MAJOR = 0 unless defined? Lobbyist::Version::MAJOR
     MINOR = 5 unless defined? Lobbyist::Version::MINOR
-    PATCH = 0 unless defined? Lobbyist::Version::PATCH
+    PATCH = 1 unless defined? Lobbyist::Version::PATCH
 
     class << self
 
