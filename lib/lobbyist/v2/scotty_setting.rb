52a2a1f7f5f6b366e35d613dae618051ffdceec0
spiritbear <indap.shiv@gmail.com>
2014-03-10 15:19:26 -0700

    getting rid of the customer_query and transaction_query

--- lib/lobbyist/v2/scotty_setting.rb
+++ lib/lobbyist/v2/scotty_setting.rb
@@ -2,7 +2,7 @@ module Lobbyist
   module V2
 
     class ScottySetting < Lobbyist::V2::Base
-      attr_accessor :id, :company_id, :customer_query, :transaction_query, :days, :time
+      attr_accessor :id, :company_id, :days, :time
       attr_accessor :upload_type, :statue, :system_enabled, :access_token, :access_secret
       attr_accessor :access_data, :link_established, :last_run_date, :authentication_date, :importer_class_name
       attr_accessor :created_at, :updated_at
