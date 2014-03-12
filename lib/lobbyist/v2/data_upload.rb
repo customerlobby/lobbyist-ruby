282929d1aaaf0a9f4fab467dec78cdcea38b5281
sreevs <sreevidya.allada@gmail.com>
2014-03-12 14:16:40 -0700

    [addressess #66900404]1) lobbyist - add list call to V2::data_upload, direst_connect_summary to V2::companies, add new proxy model Summary 2) add summary information to the direct connect page

--- lib/lobbyist/v2/data_upload.rb
+++ lib/lobbyist/v2/data_upload.rb
@@ -5,7 +5,11 @@ module Lobbyist
       attr_accessor :id, :company_id, :filename, :status, :created_at, :updated_at
 
       def self.create(params = {})
-        create_from_response(get("/v2/data_uploads.json", params))
+        create_from_response(post("/v2/data_uploads.json", params))
+      end
+
+      def self.list(params = {})
+        create_collection_from_response(get('/v2/data_uploads.json', params))
       end
     end
 
