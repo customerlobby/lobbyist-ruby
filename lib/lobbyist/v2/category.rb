7695da70e5840943a585dceca7e7a239fa66d94b
David Lains <dlains@gmail.com>
2014-04-08 14:36:40 -0700

    [addresses #69119424] Added index call to Categories.

--- lib/lobbyist/v2/category.rb
+++ lib/lobbyist/v2/category.rb
@@ -3,6 +3,11 @@ module Lobbyist
     
     class Category < Lobbyist::V2::Base
       attr_accessor :id, :name, :created_at, :updated_at
+
+      def self.list(params = {})
+        create_collection_from_response(get('/v2/categories.json', params))
+      end
+  
     end
     
   end
