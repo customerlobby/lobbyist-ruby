f20df0e796e88cae70f285cabbe195eb254ce9b5
sreevs <sreevidya.allada@gmail.com>
2014-05-08 14:08:21 -0700

    [addressess #70535966]1) updated route to api call 2)fix bug related to handling the pagination

--- lib/lobbyist/v2/appointment.rb
+++ lib/lobbyist/v2/appointment.rb
@@ -5,16 +5,16 @@ module Lobbyist
       attr_accessor :id, :company_id, :name, :first_name, :last_name, :email, :phone, :appointment_date1, 
       :appointment_date2, :appointment_date3, :comments, :created_at, :updated_at
 
-      def self.list(params = {})
-        create_collection_from_response(get("/v2/appointments.json", params))
+      def self.list(company_id, params = {})
+        create_collection_from_response(get("/v2/companies/#{company_id}/appointments.json", params))
       end
     
       def self.find(id, company_id)
-        create_from_response(get("/v2/appointments/#{id}.json"))
+        create_from_response(get("/v2/companies/#{company_id}/appointments#{id}.json"))
       end
 
       def self.update(id, company_id)
-        create_from_response(put("/v2/appointments/#{id}.json"))
+        create_from_response(put("/v2/companies/#{company_id}/appointments#{id}.json"))
       end 
     end
 
