2eeef50882e9f19283693b936e56ae1f4342943a
sreevs <sreevidya.allada@gmail.com>
2014-05-08 13:24:28 -0700

    [addressess #68458194]change column sequence in data table, date formats adjusted, used glyphicon for time period selected, only display resend btn for pending and bounced confirmation status

--- lib/lobbyist/v2/handwritten_review_metrics.rb
+++ lib/lobbyist/v2/handwritten_review_metrics.rb
@@ -4,7 +4,7 @@ module Lobbyist
     class HandwrittenReviewMetrics < Lobbyist::V2::Base
       attr_accessor :available, :used, :next_grant_date, :monthly_data
 
-      def self.find_by_company(company_id)
+      def self.list(company_id)
         create_from_response(get("/v2/companies/#{company_id}/handwritten_review_metrics.json"))
       end
  
