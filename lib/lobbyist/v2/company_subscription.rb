2a1af6ffe75a74d88955bf88e2a6178e7f4855e8
David Lains <dlains@gmail.com>
2014-05-13 14:35:55 -0700

    [addresses #69738282] Added necessary models for customer call credit handling.

---
+++ lib/lobbyist/v2/company_subscription.rb
@@ -0,0 +1,23 @@
+module Lobbyist
+  module V2
+
+    class CompanySubscription < Lobbyist::V2::Base
+      attr_accessor :id, :company_id, :subscription_id, :reference, :authorization_code
+      attr_accessor :status, :on_hold_since, :last_four, :expiration_date, :card_type
+      attr_accessor :total_payments, :next_payment_date, :last_contacted_at, :created_at, :updated_at
+      
+      def self.find(company_id)
+        create_from_response(get("/v2/companies/#{company_id}/company_subscription.json"))
+      end
+    
+      def self.create(company_id, params = {})
+        create_from_response(post("/v2/companies/#{company_id}/company_subscription.json", {company_subscription: params}))
+      end
+    
+      def self.update(company_id, params = {})
+        create_from_response(put("/v2/companies/#{company_id}/company_subscription.json", {company_subscription: params}))
+      end
+    end
+    
+  end
+end
