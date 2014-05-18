5d745ea21aef2c8189a9a091498f5f6b552329a5
David Lains <dlains@gmail.com>
2014-05-18 16:55:43 -0700

    [addresses #71335014] Company transaction model added.

---
+++ lib/lobbyist/v2/company_transaction.rb
@@ -0,0 +1,19 @@
+module Lobbyist
+  module V2
+
+    class CompanyTransaction < Lobbyist::V2::Base
+      attr_accessor :id, :company_id, :event_type, :event_description, :source
+      attr_accessor :status, :status_description, :requiest_id, :reconciliation_id, :reconciled
+      attr_accessor :amount, :transaction_date, :created_at, :updated_at
+      
+      def self.list(company_id, params = {})
+        create_collection_from_response(get("/v2/companies/#{company_id}/company_transactions.json", params))
+      end
+    
+      def self.create(company_id, params = {})
+        create_from_response(post("/v2/companies/#{company_id}/company_transactions.json", {company_transaction: params}))
+      end
+
+    end
+  end
+end
