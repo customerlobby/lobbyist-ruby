f435d2ed33de71f374a33a918d17f462630213d7
David Lains <dlains@gmail.com>
2014-05-28 15:06:57 -0700

    [addresses #71976454] Added attributes for Company and Enterprise metrics.

--- lib/lobbyist/v2/company_metrics.rb
+++ lib/lobbyist/v2/company_metrics.rb
@@ -3,6 +3,12 @@ module Lobbyist
 
     class CompanyMetrics < Lobbyist::V2::Base
 
+      attr_accessor :id, :company_id, :sentiment_score, :published_reviews_count
+      attr_accessor :positive_reviews_count, :negative_reviews_count, :distribution_by_medium
+      attr_accessor :distribution_by_source, :distribution_by_rating, :appointments_count
+      attr_accessor :customers_count, :customers_last_12_count, :average_reading_time
+      attr_accessor :review_page_views, :site_page_views, :recent_reviews, :syndication_statuses
+
       def self.list(company_id, params = {})
         create_from_response(get("/v2/companies/#{company_id}/company_metrics.json", params))
       end
