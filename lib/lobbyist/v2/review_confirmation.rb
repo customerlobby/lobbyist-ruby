225b2f35d4779f6959a438f9b0eeb71b49437353
David Lains <dlains@gmail.com>
2014-06-17 11:58:30 -0700

    [addresses #72921548] Rename kiosk review model.
    Renamed the kiosk review model to review confirmation.

---
+++ lib/lobbyist/v2/review_confirmation.rb
@@ -0,0 +1,18 @@
+module Lobbyist
+  module V2
+
+    class ReviewConfirmation < Lobbyist::V2::Base
+      attr_accessor :id, :review_id, :confirmation_type, :attempts, :max_attempts, :status
+      attr_accessor :from_phone_number, :to_phone_number, :from_ip_address, :delivered_at, :created_at, :updated_at
+
+      def self.list(company_id, params = {})
+        create_collection_from_response(get("/v2/companies/#{company_id}/review_confirmations.json", params))
+      end
+
+      def self.reset(id, company_id, params = {})
+        create_from_response(put("/v2/companies/#{company_id}/review_confirmations/#{id}/reset.json", {contact: params}))
+      end
+    end
+
+  end
+end
