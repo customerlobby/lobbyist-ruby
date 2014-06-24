aefec0f6fe6b41cdfe7d5c941cf25d5fca3e002b
Joshua Volz <jdvolz@gmail.com>
2014-06-24 15:03:28 -0700

    [Addresses #68899746]Add create/find/update functionality to WorkflowSystem

--- lib/lobbyist/v2/workflow_system.rb
+++ lib/lobbyist/v2/workflow_system.rb
@@ -1,14 +1,24 @@
 module Lobbyist
   module V2
-
     class WorkflowSystem < Lobbyist::V2::Base
-      attr_accessor :id, :name, :version, :status, :database_name, :access_type
+      attr_accessor :id, :name, :version, :status, :database_name, :access_type, :visible
       attr_accessor :assembly_location, :assembly_version, :created_at, :updated_at
 
       def self.list(params = {})
         create_collection_from_response(get("/v2/workflow_systems.json", params))
       end
-    end
 
+      def self.create(params = {})
+        create_from_response(post("/v2/workflow_systems.json", {"workflow_system"=>params}))
+      end
+
+      def self.find(workflow_system_id)
+        create_from_response(get("/v2/workflow_systems/#{workflow_system_id}.json"))
+      end
+
+      def self.update(id, params = {})
+        create_from_response(put("/v2/workflow_systems/#{id}.json", {'workflow_system' => params}))
+      end
+    end
   end
 end
