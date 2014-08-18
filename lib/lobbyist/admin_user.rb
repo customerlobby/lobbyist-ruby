f1c68a5845ae3d3189a1f3c74f3242e141afd6c7
Rob Hunt <rhunt@customerlobby.com>
2014-08-18 09:54:00 -0700

    [addresses #76491166] - added employment_type to admin user

--- lib/lobbyist/admin_user.rb
+++ lib/lobbyist/admin_user.rb
@@ -1,7 +1,7 @@
 module Lobbyist
   class AdminUser < Lobbyist::Base
     attr_accessor :admin_user_id, :email, :first_name, :last_name, :password, :is_total_admin, 
-    :is_active, :screen_name, :sales_team_id, :created_at, :updated_at, :auth_key, :persistence_token, :role
+    :is_active, :screen_name, :sales_team_id, :created_at, :updated_at, :auth_key, :persistence_token, :role, :employment_type
 
     def self.list(params = {})
       create_collection_from_response(get('/v1/admin_users.json', params))
