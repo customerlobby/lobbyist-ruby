e2ed9e34b10a721c40dc315d2c96972568d9528f
David Lains <dlains@gmail.com>
2014-09-10 14:08:25 -0700

    [addresses #78415812] Fixed email setting update.
    The update call was using the wrong id value for the enterprise. Fixed the id so the lookups work on the API side.

--- lib/lobbyist/v2/enterprise_email_setting.rb
+++ lib/lobbyist/v2/enterprise_email_setting.rb
@@ -9,7 +9,7 @@ module Lobbyist
       end
       
       def self.update(enterprise_id, id, params)
-        create_from_response(put("/v2/enterprises/#{id}/enterprise_email_settings/#{id}.json", {enterprise_email_setting: params}))
+        create_from_response(put("/v2/enterprises/#{enterprise_id}/enterprise_email_settings/#{id}.json", {enterprise_email_setting: params}))
       end
     end
 
