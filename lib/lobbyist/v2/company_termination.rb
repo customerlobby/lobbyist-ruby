1f907e951f9d7cfadf2fa3900ded195d42478077
sreevs <sreevidya.allada@gmail.com>
2014-10-02 00:52:12 -0700

    [addressess #78215852] add functionality to save a termination on accounts

--- lib/lobbyist/v2/company_termination.rb
+++ lib/lobbyist/v2/company_termination.rb
@@ -18,6 +18,10 @@ module Lobbyist
         create_from_response(put("/v2/company_terminations/#{id}.json", {company_termination: params}))
       end
 
+      def self.save_company(id)
+        create_from_response(put("/v2/company_terminations/#{id}/save-company.json"))
+      end
+
     end
   end
 end
