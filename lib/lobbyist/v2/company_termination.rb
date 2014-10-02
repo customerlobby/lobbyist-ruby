6e7d261d419004832a70616c3d913da0ceb72621
spiritbear <indap.shiv@gmail.com>
2014-10-02 15:15:32 -0700

    Adding company_termination back

---
+++ lib/lobbyist/v2/company_termination.rb
@@ -0,0 +1,27 @@
+module Lobbyist
+  module V2
+
+    class CompanyTermination < Lobbyist::V2::Base
+      attr_accessor :id, :company_id, :company_name, :company_account_class, :admin_user_id, :status, :saved
+      attr_accessor :pitch, :cancellation_channel, :projected_termination_date, :reason     
+      attr_accessor :closed_at, :transaction_date, :created_at, :updated_at
+      
+      def self.list(params = {})
+        create_collection_from_response(get("/v2/company_terminations.json", params))
+      end
+      
+      def self.find(id)
+        create_from_response(get("/v2/company_terminations/#{id}.json"))
+      end
+
+      def self.update(id, params = {})
+        create_from_response(put("/v2/company_terminations/#{id}.json", {company_termination: params}))
+      end
+
+      def self.save_company(id)
+        create_from_response(put("/v2/company_terminations/#{id}/save-company.json"))
+      end
+
+    end
+  end
+end
