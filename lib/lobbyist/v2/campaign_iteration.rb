b3ee69e60da92ebc8a03aea217b9e107f2d1c0c5
sreevs <sreevidya.allada@gmail.com>
2014-10-23 14:36:16 -0700

    [addressess #81142356] UI to show the communication_pieces for a particular campaign

---
+++ lib/lobbyist/v2/campaign_iteration.rb
@@ -0,0 +1,21 @@
+module Lobbyist
+  module V2
+
+    class DataUpload < Lobbyist::V2::Base
+      attr_accessor :id, :company_id, :filename, :status, :created_at, :updated_at
+
+      def self.create(params = {})
+        create_from_response(post("/v2/data_uploads.json", params))
+      end
+
+      def self.list(params = {})
+        create_collection_from_response(get('/v2/data_uploads.json', params))
+      end
+
+      def self.process_file(id, params = {})
+        create_from_response(put("/v2/data_uploads/#{id}/process-file.json", params))
+      end
+    end
+
+  end
+end
