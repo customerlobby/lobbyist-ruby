443929181d004c9ac97db77ff58d7c0888f615d6
Rob Hunt <rhunt@customerlobby.com>
2014-10-28 14:29:52 -0700

    [addresses #81142748] - added CustomerTransaction

---
+++ lib/lobbyist/v2/customer_transaction.rb
@@ -0,0 +1,15 @@
+module Lobbyist
+  module V2
+
+    class CustomerTransaction < Lobbyist::V2::Base
+      attr_accessor :id, :communication_piece_id, :uuid, :contact_id, :external_id, :receipt_number, :category,
+                    :event, :due_date, :amount, :is_paid, :transacted_at, :created_at, :updated_at
+
+      def self.list(id, params = {})
+        create_collection_from_response(get("/v2/campaign_iterations/#{id}/transactions.json", params = {}))
+      end
+
+    end
+
+  end
+end
