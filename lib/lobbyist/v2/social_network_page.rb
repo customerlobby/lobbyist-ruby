87deac065e36aa5586a34968353a39e91877d51a
David Lains <dlains@gmail.com>
2014-11-06 12:33:25 -0800

    [addresses #58277344] Updated api endpoints.
    Also added the facebook reset method.

--- lib/lobbyist/v2/social_network_page.rb
+++ lib/lobbyist/v2/social_network_page.rb
@@ -4,8 +4,12 @@ module Lobbyist
     class SocialNetworkPage < Lobbyist::V2::Base
       attr_accessor :id, :company_id, :social_network_profile_id, :status, :created_at, :updated_at
 
-      def self.update(company_id, page_id, params = {})
-        create_from_response(put("/v2/companies/#{company_id}/social_network_pages/#{page_id}.json", params))
+      def self.create(params = {})
+        create_from_response(post("/v2/social-network-pages.json", {social_network_page: params}))
+      end
+
+      def self.update(page_id, params = {})
+        create_from_response(put("/v2/social-network-pages/#{page_id}.json", params))
       end
     end
 
