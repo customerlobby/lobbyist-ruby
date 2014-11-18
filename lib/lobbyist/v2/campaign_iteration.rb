747e80c17030d794e046fea098e3eec81a0c5c83
Rob Hunt <rhunt@customerlobby.com>
2014-11-18 14:12:16 -0800

    [addresses #82507246] - support campaign_iterations.communication_pieces

--- lib/lobbyist/v2/campaign_iteration.rb
+++ lib/lobbyist/v2/campaign_iteration.rb
@@ -9,5 +9,11 @@ module Lobbyist
         create_collection_from_response(get("/v2/campaigns/#{campaign_id}/iterations.json", params))
       end
     end
+
+    def self.communication_pieces(id, params={})
+      response = get("v2/campaign-iterations/#{id}/communication-pieces.json", params)
+      create_collection_from_response(response)
+    end
+
   end
 end
