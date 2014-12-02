7cc4c0ed7de42183df6e25f04625c03e42f7e7a7
Rob Hunt <rhunt@customerlobby.com>
2014-12-02 13:17:54 -0800

    [addresses #79713928] - added facebook_long_url and facebook_short_url attributes

--- lib/lobbyist/smart_invite_setting.rb
+++ lib/lobbyist/smart_invite_setting.rb
@@ -2,7 +2,7 @@ module Lobbyist
   class SmartInviteSetting < Lobbyist::Base
 
     attr_accessor :id, :company_id, :enabled, :google_long_url, :google_short_url,
-      :yelp_long_url, :yelp_short_url, :created_at, :updated_at
+      :yelp_long_url, :yelp_short_url, :facebook_long_url, :facebook_short_url, :created_at, :updated_at
 
   end
 end
