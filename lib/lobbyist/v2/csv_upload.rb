d000f9c9e664039f1c4c691946d7a27ef51600c5
Joshua Volz <jvolz@customerlobby.com>
2014-12-24 20:28:33 -0800

    [Addresses #81524852]CsvUpload: add list and process_file methods
    Why: for use with Admin area and CSV file processing.

--- lib/lobbyist/v2/csv_upload.rb
+++ lib/lobbyist/v2/csv_upload.rb
@@ -3,11 +3,19 @@ module Lobbyist
 
     class CsvUpload < Lobbyist::V2::Base
       attr_accessor :id, :company_id, :filename, :status, :created_at, :updated_at
+      attr_accessor :customers_count
 
       def self.create(company_id, params = {})
-        create_from_response(post("/v2/companies/#{company_id}/csv_uploads.json", {csv_upload: params}))
+        create_from_response(post("/v2/companies/#{company_id}/csv-uploads.json", {csv_upload: params}))
+      end
+
+      def self.list(company_id, params = {})
+        create_collection_from_response(get("/v2/companies/#{company_id}/csv-uploads.json", params))
+      end
+
+      def self.process_file(id, params = {})
+        create_from_response(put("/v2/csv_uploads/#{id}/process-file.json", params))
       end
     end
-
   end
 end
