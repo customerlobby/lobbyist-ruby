024533b9b747207c962cc6307d1a83d3e8ba9bbc
David Lains <dlains@gmail.com>
2015-01-22 09:23:30 -0800

    Added payment fields to original CompanySubscription.

--- lib/lobbyist/company_subscription.rb
+++ lib/lobbyist/company_subscription.rb
@@ -2,7 +2,8 @@ module Lobbyist
   class CompanySubscription < Lobbyist::Base
     attr_accessor :id, :company_id, :subscription_id, :reference, :authorization_code
     attr_accessor :status, :on_hold_since, :last_four, :expiration_date, :card_type
-    attr_accessor :last_contacted_at, :next_payment_date, :created_at, :updated_at
+    attr_accessor :total_payments, :last_contacted_at, :next_payment_date, :created_at, :updated_at
+    attr_accessor :monthly_fee, :setup_fee, :cost_per_postcard, :customer_count_lower_bound, :customer_count_upper_bound
     
     def self.list(params = {})
       create_collection_from_response(get("/v1/company_subscriptions.json", params))
