a014b82bdcec62f138c9ae0d17e438b2c59c2f78
Joshua Volz <jvolz@customerlobby.com>
2015-02-24 14:14:47 -0800

    [Addresses #88930398]CampaignPricing Change variable name to match

--- lib/lobbyist/v2/campaign_pricing.rb
+++ lib/lobbyist/v2/campaign_pricing.rb
@@ -4,7 +4,7 @@ module Lobbyist
       attr_accessor :price, :max_price
 
       def self.find(company_id)
-        create_from_response(get("/v2/companies/#{id}/campaign-pricing.json"))
+        create_from_response(get("/v2/companies/#{company_id}/campaign-pricing.json"))
       end
     end
   end
