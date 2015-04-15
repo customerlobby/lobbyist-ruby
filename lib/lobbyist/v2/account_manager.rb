3948b2f1ffc8d1b0bb778db7cdddc83028d685be
Rob Hunt <rhunt@customerlobby.com>
2015-04-14 17:52:08 -0700

    [addresses #91891660] - fixed route

--- lib/lobbyist/v2/account_manager.rb
+++ lib/lobbyist/v2/account_manager.rb
@@ -6,7 +6,7 @@ module Lobbyist
       attr_accessor :summary_result
 
       def self.member_calls_summary(admin_user_id)
-        response = get("/v2/account_managers/#{admin_user_id}/member_calls_summary.json")
+        response = get("/v2/account-managers/#{admin_user_id}/member-calls-summary.json")
         create_from_response(response)
       end
 
