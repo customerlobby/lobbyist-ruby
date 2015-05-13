d674ffa75f4dddf495be761fae6bf1e86d8d81c1
Rob Hunt <rhunt@customerlobby.com>
2015-05-13 11:06:44 -0700

    [addresses #93582684] - added missing attribute

--- lib/lobbyist/v2/account_manager_metrics.rb
+++ lib/lobbyist/v2/account_manager_metrics.rb
@@ -4,7 +4,7 @@ module Lobbyist
     class AccountManagerMetrics < Lobbyist::V2::Base
       attr_accessor :all_count, :active_count, :inactive_count, :terminated_count, :pending_termination_count
       attr_accessor :on_hold_count, :skipped_payment_count, :direct_connect_issues_count, :first_call_count
-      attr_accessor :calandared_today_count, :not_calandared_count
+      attr_accessor :calandared_today_count, :not_calandared_count, :account_managers
 
       def self.company_filter_counts(id, params = {})
         create_from_response(get("/v2/account-manager-metrics/#{id}/company-filter-counts.json", params))
