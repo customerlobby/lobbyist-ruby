7d7cf981e9dd75016d0530c6074465d69732b238
jamesmarfa <jamesmarfa@gmail.com>
2015-07-18 20:37:31 +0800

    [Addressed #99247440] Add for index page

--- lib/lobbyist/v2/category.rb
+++ lib/lobbyist/v2/category.rb
@@ -2,7 +2,8 @@ module Lobbyist
   module V2
     
     class Category < Lobbyist::V2::Base
-      attr_accessor :id, :name, :created_at, :updated_at
+      attr_accessor :id, :name, :default_services, :repeat_marketing_campaign_enabled,
+                    :threshold, :min_days_between_mailing, :created_at, :updated_at
 
       def self.list(params = {})
         create_collection_from_response(get('/v2/categories.json', params))
