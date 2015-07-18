06277b8c7b7e3ed7e18be777a6df15a72709e720
jamesmarfa <jamesmarfa@gmail.com>
2015-07-19 02:14:43 +0800

    Added more fields in category

--- lib/lobbyist/v2/category.rb
+++ lib/lobbyist/v2/category.rb
@@ -2,8 +2,9 @@ module Lobbyist
   module V2
     
     class Category < Lobbyist::V2::Base
-      attr_accessor :id, :name, :default_services, :repeat_marketing_campaign_enabled,
-                    :threshold, :min_days_between_mailing, :created_at, :updated_at
+      attr_accessor :id, :name, :repeat_marketing_campaign_enabled,
+                    :threshold, :min_days_between_mailings, :roi, :accelerator, :mail_delay_offset,
+                    :transaction_grouping_interval, :generate_marketing_insights, :created_at, :updated_at
 
       def self.list(params = {})
         create_collection_from_response(get('/v2/categories.json', params))
