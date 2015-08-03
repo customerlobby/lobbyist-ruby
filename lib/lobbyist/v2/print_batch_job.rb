e3633a0274dfcefdf3b54b112129ec86f4707ce5
Zuzanna Stolińska <zuzanna.st@gmail.com>
2015-08-03 12:09:10 +0200

    [Adresses #99654682] Add list method to V2/print_batch_job.

--- lib/lobbyist/v2/print_batch_job.rb
+++ lib/lobbyist/v2/print_batch_job.rb
@@ -1,6 +1,11 @@
 module Lobbyist
   module V2
     class PrintBatchJob < Lobbyist::V2::Base
+      attr_accessor :id, :communication_campaign_id, :status, :created_at, :tracking_start_date, :tracking_end_date
+
+      def self.list(params = {})
+        create_collection_from_response(get("/v2/print-batch-jobs.json", params))
+      end
 
       def self.communication_pieces(id, params={})
         response = get("v2/print-batch-jobs/#{id}/communication-pieces.json", params)
