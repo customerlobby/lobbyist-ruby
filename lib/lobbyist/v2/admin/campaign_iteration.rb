f8e8d2e67d4efc735df7035aa78aa42a326121f4
jamesmarfa <jamesmarfa@gmail.com>
2015-08-09 13:14:13 +0800

    [Addressed #99661660] Added Admin campaign iteration

---
+++ lib/lobbyist/v2/admin/campaign_iteration.rb
@@ -0,0 +1,14 @@
+module Lobbyist
+  module V2
+    module Admin
+      class CampaignIteration < Lobbyist::V2::Base
+        attr_accessor :id, :communication_campaign_id, :start_date, :end_date, :tracking_start_date, :tracking_end_date
+        attr_accessor :status, :sent_count, :revenue_generated, :roi, :amount_spent, :budget_snapshot, :created_at, :updated_at
+
+        def self.list(params = {})
+          create_collection_from_response(get("/v2/admin/campaign_iterations.json", params))
+        end
+      end
+    end
+  end
+end
