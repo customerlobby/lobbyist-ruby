dd72f52c1ef1f4c1b34214647fbe261d055a2156
Joshua Volz <jvolz@customerlobby.com>
2015-08-12 08:48:50 -0700

    [Addresses #95989112]Remove resetting of params in CustomerTransaction list
    Why: it was setting it to empty params for every value of params you pass
    in.

--- lib/lobbyist/v2/customer_transaction.rb
+++ lib/lobbyist/v2/customer_transaction.rb
@@ -6,7 +6,7 @@ module Lobbyist
                     :event, :due_date, :amount, :is_paid, :transacted_at, :created_at, :updated_at
 
       def self.list(id, params = {})
-        create_collection_from_response(get("/v2/campaign-iterations/#{id}/transactions.json", params = {}))
+        create_collection_from_response(get("/v2/campaign-iterations/#{id}/transactions.json", params))
       end
 
     end
--- lib/lobbyist/version.rb
+++ lib/lobbyist/version.rb
@@ -2,7 +2,7 @@ module Lobbyist
   class Version
     MAJOR = 0 unless defined? Lobbyist::Version::MAJOR
     MINOR = 5 unless defined? Lobbyist::Version::MINOR
-    PATCH = 36 unless defined? Lobbyist::Version::PATCH
+    PATCH = 37 unless defined? Lobbyist::Version::PATCH
 
     class << self
 
