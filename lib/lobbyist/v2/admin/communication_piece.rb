f5cfb8334bbd4afee1e44e95daba30457a28f924
jamesmarfa <jamesmarfa@gmail.com>
2015-08-26 23:08:36 +0800

    [CA-3707] Added verification_errors field communication_pieces

--- lib/lobbyist/v2/admin/communication_piece.rb
+++ lib/lobbyist/v2/admin/communication_piece.rb
@@ -3,10 +3,20 @@ module Lobbyist
     module Admin
       class CommunicationPiece < Lobbyist::V2::Base
         attr_accessor :id, :customer_first_name, :customer_last_name, :customer_address1, :customer_address2
-        attr_accessor :customer_city, :customer_state, :customer_zip, :status, :verification_errors
+        attr_accessor :customer_city, :customer_state, :customer_zip, :status
 
         def self.list(campaign_iteration_id, params = {})
           create_collection_from_response(get("/v2/admin/campaign-iterations/#{campaign_iteration_id}/communication-pieces.json", params))
+        end
+
+        def verification_errors
+          @verification_errors
+        end
+
+        def verification_errors=(attributes)
+          @verification_errors = attributes.split("\n").from(1).map do |attr|
+            attr.from(2)
+          end
         end
       end
     end
--- spec/lobbyist/v2/admin/communication_piece_spec.rb
+++ spec/lobbyist/v2/admin/communication_piece_spec.rb
@@ -24,12 +24,28 @@ describe Lobbyist::V2::Admin::CommunicationPiece do
     it { should respond_to :verification_errors }
   end
 
-  describe ".list" do
+  describe "#list" do
     it 'should return list of Communication pieces' do
       stub_get('/v2/admin/campaign-iterations/1/communication-pieces.json').to_return(body: { items: [] }.to_json, status: 200)
 
       sut = Lobbyist::V2::Admin::CommunicationPiece.list(1)
       sut.should be_a Lobbyist::Collection
+    end
+  end
+
+  describe ".verification_error" do
+    it "should return an value" do
+      sut = Lobbyist::V2::Admin::CommunicationPiece.new({verification_errors: "---\n- sample"})
+
+      expect(sut.verification_errors).to be_a Array
+    end
+  end
+
+  describe ".verification_errors=" do
+    it 'should set verification error value' do
+      sut = Lobbyist::V2::Admin::CommunicationPiece.new({})
+      sut.verification_errors = "---\n- value"
+      expect(sut.verification_errors).to eq ["value"]
     end
   end
 end
