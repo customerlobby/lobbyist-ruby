6ff724c28e81a2e456082a7f7e7b569eaf95476c
Osman Afridi <osmanafridi@Osmans-MacBook-Pro.local>
2015-09-15 15:19:17 -0700

    CA-3673 Fix url

--- lib/lobbyist/v2/recommended_budget.rb
+++ lib/lobbyist/v2/recommended_budget.rb
@@ -5,7 +5,7 @@ module Lobbyist
       attr_accessor :id, :budget, :calculated_at, :include_commercial, :include_unpaid
 
       def self.list(company_id, params = {})
-        create_collection_from_response(get("/v2/companies/#{company_id}/recommended_budgets.json", params))
+        create_collection_from_response(get("/v2/companies/#{company_id}/recommended-budgets.json", params))
       end
 
     end
