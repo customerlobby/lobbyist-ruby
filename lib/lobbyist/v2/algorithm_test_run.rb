dc652a761f4b90aca73d9c030f0bd6dd9e3fae67
David Lains <dlains@gmail.com>
2015-09-29 10:50:29 -0700

    CA-3817 Added repeat customer attribute.

--- lib/lobbyist/v2/algorithm_test_run.rb
+++ lib/lobbyist/v2/algorithm_test_run.rb
@@ -5,12 +5,12 @@ module Lobbyist
       attr_accessor :id, :name, :company_id, :status, :threshold, :budget, :minimum_roi, :accelerator, :transaction_grouping_interval
       attr_accessor :min_days_between_mailings, :mail_delay_offset, :include_commercial_customers
       attr_accessor :cost_per_postcard, :proposed_customer_count, :selected_customer_count, :recommended_budget, :best_buckets
-      attr_accessor :proposed_count_per_bucket, :ninety_day_ev_per_bucket, :created_at, :updated_at
+      attr_accessor :proposed_count_per_bucket, :ninety_day_ev_per_bucket, :created_at, :updated_at, :repeat_customer_transaction_threshold
 
       def self.list(params = {})
         create_collection_from_response(get("/v2/algorithm-test-runs.json", params))
       end
-    
+
       def self.find(id, params = {})
         create_from_response(get("/v2/algorithm-test-runs/#{id}.json", params))
       end
