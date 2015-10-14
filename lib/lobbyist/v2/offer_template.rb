05754ad8c3abfefaaa374a5f9300bf91029c5342
David Lains <dlains@gmail.com>
2015-10-14 09:29:02 -0700

    CA-3927 New fields added to existing models.

--- lib/lobbyist/v2/offer_template.rb
+++ lib/lobbyist/v2/offer_template.rb
@@ -1,8 +1,8 @@
 module Lobbyist
   module V2
-    
+
     class OfferTemplate < Lobbyist::V2::Base
-      attr_accessor :id, :communication_campaign_id, :upper_bound, :lower_bound, :content, :status, :created_at, :updated_at
+      attr_accessor :id, :communication_campaign_id, :upper_bound, :lower_bound, :content, :disclaimers, :status, :created_at, :updated_at
 
       def self.list(params = {})
         create_collection_from_response(get('/v2/offer-templates.json', params))
@@ -11,12 +11,12 @@ module Lobbyist
       def self.create(params = {})
         create_from_response(post('/v2/offer-templates.json', params))
       end
-    
+
       def self.update(id, params)
         create_from_response(put("/v2/offer-templates/#{id}.json", params))
       end
 
     end
-    
+
   end
 end
