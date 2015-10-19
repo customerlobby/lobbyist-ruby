cd9e71cc266d94670b9a18bca0911a98c870af51
jamesmarfa <jamesmarfa@gmail.com>
2015-10-19 20:14:59 +0800

    [CA-3880] Added list and destroy for V2 CustomerCallCredit

--- lib/lobbyist/v2/customer_call_credit.rb
+++ lib/lobbyist/v2/customer_call_credit.rb
@@ -2,11 +2,19 @@ module Lobbyist
   module V2
 
     class CustomerCallCredit < Lobbyist::V2::Base
+      attr_accessor :id, :event, :status
+
+      def self.list(company_id, params = {})
+        create_collection_from_response(get("/v2/companies/#{company_id}/customer_call_credits.json", params))
+      end
 
       def self.create(company_id, params = {})
         create_from_response(post("/v2/companies/#{company_id}/customer_call_credits.json", params))
       end
-      
+
+      def self.destroy(company_id, id)
+        create_from_response(delete("/v2/companies/#{company_id}/customer_call_credits/#{id}.json"))
+      end
     end
 
   end
