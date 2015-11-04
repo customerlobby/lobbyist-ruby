7d89326a86fe797de36964944e97b14a93b83e2c
Osman Afridi <osmanafridi@Osmans-MacBook-Pro.local>
2015-11-04 09:32:33 -0800

    CA-4069: Updated accessors

--- lib/lobbyist/v2/member_aggregate_insight.rb
+++ lib/lobbyist/v2/member_aggregate_insight.rb
@@ -3,8 +3,10 @@ module Lobbyist
     
     class MemberAggregateInsight < Lobbyist::V2::Base
       attr_accessor :id, :company_id, :revenue_from_repeat_customers, :revenue_from_new_customers, :customers_with_transactions
-      attr_accessor :lifetime_value_of_customers, :recommended_budget, :recommended_budget_status, :customer_contact_info_distribution
-      attr_accessor :top_five_customers, :created_at, :updated_at
+      attr_accessor :ltv, :recommended_budget, :recommended_budget_status, :customer_contact_info_distribution
+      attr_accessor :top_five_customers, :created_at, :updated_at, :avg_num_of_visits
+      attr_accessor :avg_num_of_visits_excluding_first, :new_customer_count, :residential_customer_count
+      attr_accessor :commercial_customer_count, :residential_customer_revenue, :commercial_customer_revenue
 
       def self.update(company_id, params)
         create_from_response(put("/v2/companies/#{company_id}/member-aggregate-insight.json", {member_aggregate_insight: params}))
