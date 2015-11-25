7c1117523b3fec0335c2488ac4947db53413ac9c
Osman Afridi <osmanafridi@Osmans-MacBook-Pro.local>
2015-11-25 13:28:15 -0800

    Remove trailing newline

--- deploy.rb
+++ deploy.rb
@@ -1,5 +1,7 @@
+#!/usr/bin/env ruby
+
 file_name = %x(gem build lobbyist.gemspec |  awk '/File/ {print $2}' -)
 raise "Build failed!" unless file_name
 
-command = "curl -F package=@#{file_name} https://push.fury.io/P7qkxZzYFkrcJPGnWV43/customerlobby"
+command = "curl -F package=@#{file_name.chomp} https://push.fury.io/P7qkxZzYFkrcJPGnWV43/customerlobby"
 exec(command)
