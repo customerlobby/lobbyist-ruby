d00349398e733c85c46f30fd52fc781dbae39a9e
sreevs <sreevidya.allada@gmail.com>
2015-12-21 17:07:33 -0800

    ca-4271 lobbyist changes to the corresponding insigts endpoint

--- lib/lobbyist/v2/category_insight_metrics.rb
+++ lib/lobbyist/v2/category_insight_metrics.rb
@@ -7,7 +7,7 @@ module Lobbyist
       attr_accessor :upper_quartile_ltv, :lower_quartile_percent_revenue_from_repeat_customers
       attr_accessor :median_percent_revenue_from_repeat_customers, :upper_quartile_percent_revenue_from_repeat_customers
       attr_accessor :avg_num_of_visits, :avg_num_of_visits_excluding_first, :avg_txn_value_index, :activity_index
-      attr_accessor :pricing_index, :created_at, :updated_at
+      attr_accessor :transactions_index, :created_at, :updated_at
 
       def self.insights(category_id, params = {})
         create_from_response(get("/v2/categories/#{category_id}/insights.json", params))
