798b30f943686a957a57d3c63827d10e11e5f528
Osman Afridi <osmanafridi@Osmans-MacBook-Pro.local>
2015-12-23 16:04:10 -0800

    CA-4122: Move to V2

--- lib/lobbyist/v2/call_agent_metrics.rb
+++ lib/lobbyist/v2/call_agent_metrics.rb
@@ -1,7 +1,11 @@
 module Lobbyist
-    class CallAgentMetrics < Lobbyist::Base
+  module V2
+
+    class CallAgentMetrics < Lobbyist::V2::Base
       def self.list(params = {})
         create_from_response(get('/v2/call_agent_metrics.json', params))
       end
     end
+
+  end
 end
