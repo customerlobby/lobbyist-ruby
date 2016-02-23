7d52f32bbcfadc26fa5f897d3d013e39d1b4aac6
spiritbear <indap.shiv@gmail.com>
2016-02-23 13:35:28 -0800

    Getting rid of uuid

--- lib/lobbyist/v2/communication_piece.rb
+++ lib/lobbyist/v2/communication_piece.rb
@@ -3,7 +3,7 @@ module Lobbyist
     class CommunicationPiece < Lobbyist::V2::Base
       attr_accessor :id, :communication_campaign_id, :campaign_iteration_id, :customer_first_name, 
       :customer_last_name, :customer_address1, :customer_address2, :customer_state, 
-      :customer_city, :customer_zip,  :uuid, :status, :print_batch_job_id, :tracking_code, 
+      :customer_city, :customer_zip, :status, :print_batch_job_id, :tracking_code, 
       :tracking_status, :delivered_at, :created_at, :updated_at
 
       def self.list(id, params = {})
