ab27b20e03d533bef37e419479c8e8aba5441241
David Lains <dlains@gmail.com>
2016-04-13 08:38:11 -0700

    CA-4552 Added new custom smart invite fields.

--- lib/lobbyist/v2/smart_invite_setting.rb
+++ lib/lobbyist/v2/smart_invite_setting.rb
@@ -6,6 +6,9 @@ module Lobbyist
       attr_accessor :yelp_long_url, :yelp_short_url
       attr_accessor :created_at, :updated_at, :angies_list_active, :angies_list_long_url, :angies_list_short_url
       attr_accessor :facebook_long_url, :facebook_short_url
+      attr_accessor :custom_display_name_1, :custom_long_url_1, :custom_short_url_1
+      attr_accessor :custom_display_name_2, :custom_long_url_2, :custom_short_url_2
+      attr_accessor :custom_display_name_3, :custom_long_url_3, :custom_short_url_3
 
       def self.update(company_id, params)
         create_from_response(put("/v2/companies/#{company_id}/smart_invite_settings.json", {smart_invite_setting: params}))
@@ -14,4 +17,3 @@ module Lobbyist
 
   end
 end
-
