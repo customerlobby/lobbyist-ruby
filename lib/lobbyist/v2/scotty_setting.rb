8760eb8337f4f908706accb011e88156574845cd
spiritbear <indap.shiv@gmail.com>
2016-04-27 14:02:22 -0700

    Updating the ScottySetting

--- lib/lobbyist/v2/scotty_setting.rb
+++ lib/lobbyist/v2/scotty_setting.rb
@@ -5,7 +5,8 @@ module Lobbyist
       attr_accessor :id, :company_id, :days, :time
       attr_accessor :upload_type, :system_enabled, :use_closed_invoices, :access_token, :access_secret
       attr_accessor :access_data, :link_established, :last_run_date, :authentication_date, :importer_class_name
-      attr_accessor :created_at, :updated_at, :account_name, :connection_params
+      attr_accessor :created_at, :updated_at, :account_name 
+      attr_accessor :connection_params, :connection_status, :connection_error_internal, :connection_error_external, :connection_notes 
 
       def self.create(params)
         create_from_response(post("/v2/scotty_settings.json", {scotty_setting: params}))
