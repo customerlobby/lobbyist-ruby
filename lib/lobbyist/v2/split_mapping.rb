8f1d1e3b9d9b485afc649b9862132f3bf280ae81
Kareem Ascha <kascha@Kareems-MacBook-Pro.local>
2016-06-23 10:46:36 -0700

    CA-4984 add split_mapping class

---
+++ lib/lobbyist/v2/split_mapping.rb
@@ -0,0 +1,27 @@
+module Lobbyist
+  module V2
+
+    class SplitMapping < Lobbyist::V2::Base
+
+      attr_accessor :id, :company_id, :mapped_company_id, :mapping_data, :created_at, :updated_at
+
+      def self.list(params = {})
+        create_collection_from_response("/v2/split_mappings.json", params)
+      end
+
+      def self.find(id)
+        create_from_response(get("/v2/split_mappings/#{id}.json"))
+      end
+
+      def self.create(split_mapping_params = {})
+        params = {split_mapping: split_mapping_params}
+        create_from_response(post("/v2/split_mappings.json", params))
+      end
+
+      def self.update(id, params = {})
+        create_from_response(put("/v2/split_mappings/#{id}.json", {'split_mapping' => params}))
+      end
+      
+    end
+  end
+end
