a5c94cd6be42254eb552e5f49520af08f20655e2
Osman Afridi <osmanisalso@gmail.com>
2016-07-27 11:28:56 -0700

    CA-5061:Minor formatting.

--- lib/lobbyist/promo.rb
+++ lib/lobbyist/promo.rb
@@ -1,21 +1,21 @@
 module Lobbyist
   class Promo < Lobbyist::Base
-    
-    attr_accessor :promo_id, :account_level_id, :account_class, :promo_code, :promo_description, :is_current, :is_visible, :setup_fee, :monthly_fee, 
-    :first_month_fee, :split_activation_fee, :days_available, :cost_per_postcard, :customer_count_lower_bound, :customer_count_upper_bound, :tax_exempt
-    
+
+    attr_accessor :promo_id, :account_level_id, :account_class, :promo_code, :promo_description, :is_current,
+                  :is_visible, :setup_fee, :monthly_fee, :first_month_fee, :split_activation_fee, :days_available,
+                  :cost_per_postcard, :customer_count_lower_bound, :customer_count_upper_bound, :tax_exempt
 
     def self.create(params = {})
-      create_from_response(post("/v1/promos.json", {'promo' => params}))
+      create_from_response(post('/v1/promos.json', { 'promo' => params }))
     end
-    
+
     def self.list(params = {})
       create_from_response(get('/v1/promos.json', params))
     end
 
     def self.update(id, params = {})
-      create_from_response(put("/v1/promos/#{id}.json", {'promo' => params}))
+      create_from_response(put("/v1/promos/#{id}.json", { 'promo' => params }))
     end
-    
+
   end
 end
