b1b5d015d7910edc4ecfe1fc0d0455ea97ebb48f
aschak <aschak2@gmail.com>
2016-08-09 10:45:35 -0700

    CA-5186 removed references to recommended_budget, current_balance, and monthly_budget columns

--- lib/lobbyist/version.rb
+++ lib/lobbyist/version.rb
@@ -2,7 +2,7 @@ module Lobbyist
   class Version
     MAJOR = 0 unless defined? Lobbyist::Version::MAJOR
     MINOR = 6 unless defined? Lobbyist::Version::MINOR
-    PATCH = 27 unless defined? Lobbyist::Version::PATCH
+    PATCH = 28 unless defined? Lobbyist::Version::PATCH
 
     class << self
 
