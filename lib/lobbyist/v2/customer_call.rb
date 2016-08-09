29f51c424aea6112370ff43a1f81be48105eb40f
Alexey Yesipenko <alex7y@gmail.com>
2016-08-09 06:03:33 +0300

    CA-5164 Remove CompanyCustomerCall class

--- lib/lobbyist/v2/customer_call.rb
+++ lib/lobbyist/v2/customer_call.rb
@@ -1,12 +1,19 @@
 module Lobbyist
   module V2
     class CustomerCall < Lobbyist::V2::Base
-      attr_accessor :id, :company_id, :contact_id, :customer_call_credit_id, :admin_user_id
-      attr_accessor :status, :attempts, :notes, :last_call_date, :next_call_date
-      attr_accessor :completed_at, :created_at, :updated_at
+      attr_accessor :id, :company_id, :contact_id, :customer_call_credit_id, :admin_user_id, :status
+      attr_accessor :attempts, :notes, :last_call_date, :next_call_date, :completed_at
+      attr_accessor :created_at, :updated_at
+      attr_accessor :email, :phone_alt, :phone_mobile, :phone_daytime, :full_name, :admin_name
 
-      def self.list(params = {})
-        create_collection_from_response(get('/v2/customer_calls.json', params))
+      def self.list(company_id = {}, params = {})
+        path = if company_id.is_a?(Hash)
+          params = company_id
+          "/v2/customer_calls.json"
+        else
+          "/v2/companies/#{company_id}/company_customer_calls.json"
+        end
+        create_collection_from_response(get(path, params))
       end
 
       def self.find(id)
@@ -30,6 +37,9 @@ module Lobbyist
         create_from_response(put("/v2/customer_calls/#{id}/dequeue.json"))
       end
 
+      def self.destroy(company_id, id)
+        create_from_response(delete("/v2/companies/#{company_id}/customer_calls/#{id}.json"))
+      end
     end
   end
 end
