555ab496b142ca45ef25eedc5f0a6506fa139f47
David Lains <dlains@gmail.com>
2016-09-28 16:38:26 -0700

    CA-5451 Added workflow_system_used to DataUpload.

--- lib/lobbyist/v2/data_upload.rb
+++ lib/lobbyist/v2/data_upload.rb
@@ -4,7 +4,7 @@ module Lobbyist
       attr_accessor :id, :company_id, :filename, :status, :retry_count
       attr_accessor :error_message, :data_collection_successful, :customers_count
       attr_accessor :transactions_count, :created_at, :updated_at, :vehicles_count
-      attr_accessor :mileages_count, :to_be_processed, :mapper_used
+      attr_accessor :mileages_count, :to_be_processed, :mapper_used, :workflow_system_used
 
       def self.create(params = {})
         create_from_response(post('/v2/data_uploads.json', params))
