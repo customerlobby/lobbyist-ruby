16aff082bd9391d013ae15e6c6bc190537aeb96e
Alexey Yesipenko <alex7y@gmail.com>
2016-11-09 21:45:55 +0300

    CA-5612 Fix column name

--- lib/lobbyist/v2/am_unassignable_company.rb
+++ lib/lobbyist/v2/am_unassignable_company.rb
@@ -1,7 +1,8 @@
 module Lobbyist
   module V2
     class AmUnassignableCompany < Lobbyist::V2::Base
-      attr_accessor :company_id, :company_name, :ae, :account_class, :status, :date_live
+      attr_accessor :company_id, :company_name, :account_executive
+      attr_accessor :account_class, :status, :date_live
       attr_accessor :enterprise, :reviews_count, :close_date, :admin_notes
 
       def self.list(params = {})
