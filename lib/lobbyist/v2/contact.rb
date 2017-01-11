bc0f4c6b923e99059bf1cb58616bfec36d447db3
David Lains <dlains@gmail.com>
2017-01-11 15:58:05 -0800

    CA-5874 Added support for the communication_preferences field.

--- lib/lobbyist/v2/contact.rb
+++ lib/lobbyist/v2/contact.rb
@@ -1,13 +1,15 @@
 module Lobbyist
   module V2
-
     class Contact < Lobbyist::V2::Base
+      include CommunicationPreferencesHelper
+
       attr_accessor :contact_id, :company_id, :first_name, :last_name, :phone_daytime, :phone_alt
       attr_accessor :email, :address1, :address2, :city, :state, :zip, :country, :last_service_date
       attr_accessor :notes, :wants_emails, :wants_sms, :wants_calls, :wants_direct_mail, :unsubscribe_reason
       attr_accessor :unsubscribed_at, :auto_review_id
       attr_accessor :auto_invite_id, :auto_customer_call_id, :created_at, :updated_at
-      attr_accessor :phone_daytime_ext, :phone_alt_ext, :phone_mobile, :external_id, :company_name, :commercial, :status
+      attr_accessor :phone_daytime_ext, :phone_alt_ext, :phone_mobile, :external_id, :company_name
+      attr_accessor :commercial, :status, :communication_preferences
 
       def self.find(id)
         create_from_response(get("/v2/contacts/#{id}.json"))
