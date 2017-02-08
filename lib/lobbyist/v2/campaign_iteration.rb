c8ff88649386fd1fe5d066faa83f352bfdda2235
Osman Afridi <osmanisalso@gmail.com>
2017-02-08 11:03:10 -0800

    CA-5909:Create Lobbyist object from response.

--- lib/lobbyist/v2/campaign_iteration.rb
+++ lib/lobbyist/v2/campaign_iteration.rb
@@ -3,14 +3,16 @@ module Lobbyist
 
     class CampaignIteration < Lobbyist::V2::Base
       attr_accessor :id, :communication_campaign_id, :start_date, :end_date, :tracking_start_date, :tracking_end_date
-      attr_accessor :status, :sent_count, :revenue_generated, :roi, :amount_spent, :budget_snapshot, :credit_snapshot, :created_at, :updated_at
+      attr_accessor :status, :sent_count, :revenue_generated, :roi, :amount_spent
+      attr_accessor :budget_snapshot, :credit_snapshot, :created_at, :updated_at, :results_filename
 
       def self.list(campaign_id, params = {})
         create_collection_from_response(get("/v2/campaigns/#{campaign_id}/campaign-iterations.json", params))
       end
 
       def self.generate_results_file(id, params={})
-        post("v2/campaign-iterations/#{id}/generate-file.json", params)
+        response = post("v2/campaign-iterations/#{id}/generate-file.json", params)
+        create_from_response(response)
       end
     end
 
