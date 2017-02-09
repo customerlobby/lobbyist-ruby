ef55a4b4ceb4aa11f13cbc2cdf269830c0d5c240
Osman Afridi <osmanisalso@gmail.com>
2017-02-08 16:13:37 -0800

    CA-5909:Add filename and file status.

--- lib/lobbyist/v2/campaign_iteration.rb
+++ lib/lobbyist/v2/campaign_iteration.rb
@@ -3,8 +3,8 @@ module Lobbyist
 
     class CampaignIteration < Lobbyist::V2::Base
       attr_accessor :id, :communication_campaign_id, :start_date, :end_date, :tracking_start_date, :tracking_end_date
-      attr_accessor :status, :sent_count, :revenue_generated, :roi, :amount_spent
-      attr_accessor :budget_snapshot, :credit_snapshot, :created_at, :updated_at, :results_filename
+      attr_accessor :status, :sent_count, :revenue_generated, :amount_spent, :stored_file_status
+      attr_accessor :budget_snapshot, :credit_snapshot, :created_at, :updated_at, :filename, :roi
 
       def self.list(campaign_id, params = {})
         create_collection_from_response(get("/v2/campaigns/#{campaign_id}/campaign-iterations.json", params))
