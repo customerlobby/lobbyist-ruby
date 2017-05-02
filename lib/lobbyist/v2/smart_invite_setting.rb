7ae6d4110cc7d67a6491fd6a2fde85da0a1c3315
Alexey Yesipenko <alex7y@gmail.com>
2017-05-02 21:46:47 +0300

    CA-6361 Add check of long urls for SmartInviteSetting

--- lib/lobbyist/v2/smart_invite_setting.rb
+++ lib/lobbyist/v2/smart_invite_setting.rb
@@ -10,8 +10,28 @@ module Lobbyist
       attr_accessor :custom_display_name_2, :custom_long_url_2, :custom_short_url_2
       attr_accessor :custom_display_name_3, :custom_long_url_3, :custom_short_url_3
 
+      def self.check_invalid_long_urls(obj, params)
+        urls = []
+        check_urls = {
+          google_long_url: "Google",
+          yelp_long_url: "Yelp",
+          angies_list_long_url: "Angie's List",
+          facebook_long_url: "Facebook",
+          custom_long_url_1: "Custom-1",
+          custom_long_url_2: "Custom-2",
+          custom_long_url_3: "Custom-3",
+        }.each do |key, msg|
+          valNew = obj.send(key) || ''
+          valOld = params[key.to_s]
+          urls << msg if valOld && valNew != valOld
+        end
+        raise "Some urls are invalid: #{urls.join(", ")}" unless urls.empty?
+      end
+
       def self.update(company_id, params)
-        create_from_response(put("/v2/companies/#{company_id}/smart_invite_settings.json", {smart_invite_setting: params}))
+        res = create_from_response(put("/v2/companies/#{company_id}/smart_invite_settings.json", {smart_invite_setting: params}))
+        check_invalid_long_urls(res, params)
+        res
       end
     end
 
