a2bf6efd2d141e3db15af6e32336557a4f6ba9ef
sreevidya allada <sreevidya.allada@gmail.com>
2017-09-07 14:52:34 -0700

    Revert "CA-6866 new Topic class in review"
    This reverts commit 937c2f6e0e841f0ddbb7f7e510866850acd5bad6.

--- lib/lobbyist/version.rb
+++ lib/lobbyist/version.rb
@@ -2,7 +2,7 @@ module Lobbyist
   class Version
     MAJOR = 0 unless defined? Lobbyist::Version::MAJOR
     MINOR = 7 unless defined? Lobbyist::Version::MINOR
-    PATCH = 40 unless defined? Lobbyist::Version::PATCH
+    PATCH = 39 unless defined? Lobbyist::Version::PATCH
 
     class << self
       # @return [String]
