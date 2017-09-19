85aff830f79a851011ba1908bf07ac1d5fcba580
Chaitanya Jawale <chaitanya@opcito.com>
2017-09-19 14:52:41 +0530

    CA-6912 #comment 1.modified lobbyist data_warehouse job update api

--- lib/lobbyist/v2/data_warehouse_job.rb
+++ lib/lobbyist/v2/data_warehouse_job.rb
@@ -1,13 +1,13 @@
 module Lobbyist
   module V2
-
     class DataWarehouseJob < Lobbyist::V2::Base
-      attr_accessor :id, :company_id, :status, :params, :data_filename, :result_filename, :job_type, :completed_at
+      attr_accessor :id, :company_id, :status,
+                    :params, :data_filename, :result_filename,
+                    :job_type, :completed_at
 
-      def self.update_status(data_warehouse_job_id, status)
-
-        create_from_response(post("/v2/data_warehouse_job/#{data_warehouse_job_id}/status.json",
-                                  {data_warehouse_job_id: data_warehouse_job_id, status: status}))
+      def self.update(id, params)
+        create_from_response(
+            put("/v2/data_warehouse_job/#{id}.json", params))
       end
     end
   end
