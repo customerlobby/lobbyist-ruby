fa8043de8596dcc576468ec2f84a7f57c6ce79d1
Chaitanya Jawale <chaitanya@opcito.com>
2017-10-17 12:20:36 -0700

    CA-7076 #comment Fixed all review comments, added specs for direct connect import counts and updated CHANGELOG.

--- spec/lobbyist/v2/company_spec.rb
+++ spec/lobbyist/v2/company_spec.rb
@@ -10,4 +10,14 @@ describe Lobbyist::V2::Company, customer_call: true do
       end
     end
   end
+
+  describe "#direct_connect_import_counts" do
+    it 'get direct connect import counts for a company' do
+      VCR.use_cassette('v2/direct_connect_import_counts') do
+        results = Lobbyist::V2::Company.direct_connect_import_counts(5565)
+        expect(results).to be_a(Lobbyist::V2::DirectConnectImportCount)
+        expect(results.company_id).to be(5565)
+      end
+    end
+  end
 end
