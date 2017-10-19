a4950dbf4722179417e68fee54319d89e20ecc98
Chaitanya Jawale <chaitanya@opcito.com>
2017-10-19 10:33:53 -0700

    CA-7076 #comment Some additional specs to cover functionaity better.

--- spec/lobbyist/v2/company_spec.rb
+++ spec/lobbyist/v2/company_spec.rb
@@ -19,5 +19,31 @@ describe Lobbyist::V2::Company, customer_call: true do
         expect(results.company_id).to be(5565)
       end
     end
+
+    it 'get direct connect import counts as 0 for a company' do
+      VCR.use_cassette('v2/direct_connect_import_counts_0') do
+        results = Lobbyist::V2::Company.direct_connect_import_counts(40497)
+        expect(results).to be_a(Lobbyist::V2::DirectConnectImportCount)
+        expect(results.company_id).to be(40497)
+        expect(results.customers).to be(0)
+        expect(results.transactions).to be(0)
+        expect(results.vehicles).to be(0)
+        expect(results.mileages).to be(0)
+        expect(results.service_contracts).to be(0)
+      end
+    end
+
+    it 'get direct connect import counts for a company and validate all counts' do
+      VCR.use_cassette('v2/direct_connect_import_counts_validate') do
+        results = Lobbyist::V2::Company.direct_connect_import_counts(40365)
+        expect(results).to be_a(Lobbyist::V2::DirectConnectImportCount)
+        expect(results.company_id).to be(40365)
+        expect(results.customers).to be(111)
+        expect(results.transactions).to be(222)
+        expect(results.vehicles).to be(333)
+        expect(results.mileages).to be(444)
+        expect(results.service_contracts).to be(555)
+      end
+    end
   end
 end
