e41b820af6cf95216ca1a308769c5b599b2817d5
sreevidya allada <sreevidya.allada@gmail.com>
2017-11-01 16:01:41 -0700

    CA-7049 testcase and some changes

--- lib/lobbyist/v2/global_campaign_setting.rb
+++ lib/lobbyist/v2/global_campaign_setting.rb
@@ -1,26 +1,19 @@
 module Lobbyist
   module V2
 
-    class AccountLevel < Lobbyist::V2::Base
-      attr_accessor :account_class_id, :account_class, :description, :call_credit_cost, :default_promo_id
-      attr_accessor :enabled, :monthly_call_credit_allowance, :ccall_credits_accumulate
-      attr_accessor :monthly_handwritten_credit_allowance, :handwritten_credits_accumulate
-      attr_accessor :conversion_handwritten_credit_grant, :conversion_ccall_credit_grant
-      attr_accessor :paid, :rank, :created_at, :updated_at, :monthly_fee, :setup_fee, :cost_per_postcard
-      attr_accessor :customer_count_lower_bound, :customer_count_upper_bound, :has_review_reminders
-      attr_accessor :has_contact_reminders, :has_member_calls, :has_kiosk, :has_customer_calls
-      attr_accessor :has_handwritten_reviews, :supported, :visible, :internal, :requires_button, :campaign_enabled
+    class GlobalCampaignSetting < Lobbyist::V2::Base
+      attr_accessor :company_id, :logo_filename, :tag_line, :regulatory_info, :review_type
 
       def self.list(params = {})
-        create_collection_from_response(get("/v2/account_levels.json", params))
+        create_collection_from_response(get("/v2/global_campaign_settings.json", params))
       end
 
-      def self.find(id, params = {})
-        create_from_response(get("/v2/account_levels/#{id}.json", params))
+      def self.find_by_company(company_id, params = {})
+        create_from_response(get("/v2/global_campaign_settings/find-by-company/#{company_id}.json", params))
       end
 
       def self.update(id, params = {})
-        create_from_response(put("/v2/account_levels/#{id}.json", params))
+        create_from_response(put("/v2/global_campaign_settings/#{id}.json", params))
       end
     end
 
