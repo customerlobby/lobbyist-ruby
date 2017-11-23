161deca271649f0200aaab44b0b0b850ae6118eb
Chaitanya Jawale <chaitanya@opcito.com>
2017-11-23 13:44:41 +0530

    CA-7238 #comment Reverting unwanted changes from base.rb.

--- lib/lobbyist/v2/base.rb
+++ lib/lobbyist/v2/base.rb
@@ -35,13 +35,6 @@ module Lobbyist
             list << self.new(element)
           end
           return list
-        elsif response[response.keys.first.to_s].is_a?(Array)
-          list = []
-          response[response.keys.first.to_s].each do |element|
-            list << Object.const_get("Lobbyist").const_get("V2").const_get(response.keys.first.classify).new(element)
-          end
-          return list
-        end
       end
 
       def self.build_entity(data)
