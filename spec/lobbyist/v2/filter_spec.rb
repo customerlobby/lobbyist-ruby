c1c990bf285ba8923ee011b0de98df11add4dc8b
sreevidya allada <sreevidya.allada@gmail.com>
2017-12-07 01:20:25 -0800

    CA-7106 fix test case related to filters

--- spec/lobbyist/v2/filter_spec.rb
+++ spec/lobbyist/v2/filter_spec.rb
@@ -4,10 +4,10 @@ describe Lobbyist::V2::Filter do
   describe "#list" do
     it "should return list of filters" do
       VCR.use_cassette('v2/filters_list') do
-        filters = Lobbyist::V2::Filter.list(5565)
+        filters = Lobbyist::V2::Filter.list(company_id: 45485)
 
-        expect(filters.elements).to_not be_blank
-        expect(filters.elements.count).to eq(13)
+        expect(filters).to_not be_blank
+        expect(filters.count).to eq(15)
       end
     end
   end
