b69f9797eb2cc9be179af46b38b42d61101296f5
Chaitanya Jawale <chaitanya@opcito.com>
2018-02-02 12:16:06 +0530

    CA-7410 #comment 1.Added changelog and updated lobbyist version

--- lib/lobbyist/version.rb
+++ lib/lobbyist/version.rb
@@ -2,7 +2,7 @@ module Lobbyist
   class Version
     MAJOR = 0 unless defined? Lobbyist::Version::MAJOR
     MINOR = 7 unless defined? Lobbyist::Version::MINOR
-    PATCH = 64 unless defined? Lobbyist::Version::PATCH
+    PATCH = 65 unless defined? Lobbyist::Version::PATCH
 
     class << self
       # @return [String]
