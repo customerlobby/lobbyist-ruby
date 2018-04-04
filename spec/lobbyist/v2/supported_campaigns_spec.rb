23c3dc707a45925037721bb189d5662e5888d89f
sreevidya allada <sreevidya.allada@gmail.com>
2018-04-04 15:30:57 -0700

    CA-7764 remove a debug statement

--- spec/lobbyist/v2/supported_campaigns_spec.rb
+++ spec/lobbyist/v2/supported_campaigns_spec.rb
@@ -5,7 +5,6 @@ describe Lobbyist::V2::SupportedCampaigns do
     it 'list all supported_campaigns' do
       VCR.use_cassette('v2/supported_campaigns') do
         result = Lobbyist::V2::SupportedCampaigns.list(company_id: 239)
-        p result.to_s
         expect(result).not_to be(nil)
         expect(result.campaign_categories).to eq(["drip", "blast", "branding"])
         expect(result.filters.count > 0).to be_truthy
