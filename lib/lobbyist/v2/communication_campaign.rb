88f887d8068cd838ca04669f8685a706ffbbebc4
Sachin S Wagh <sachin.wagh@opcito.com>
2018-04-24 19:18:13 +0530

    CA-7757 # Comment Lobbyist: Added method to create_branding_campaign.

--- lib/lobbyist/v2/communication_campaign.rb
+++ lib/lobbyist/v2/communication_campaign.rb
@@ -6,7 +6,7 @@ module Lobbyist
         :accelerator, :transaction_grouping_interval, :min_days_between_mailings, :ignore_thirty_day_bucket, :status, :campaign_type,
         :last_run_date, :run_count, :call_to_action_tracking, :include_commercial_customers, :include_customers_in_contract, :services_offered, :logo_filename,
         :monthly_budget, :recommended_budget, :current_balance, :tag_line, :regulatory_info, :email_first_name, :email_last_name, :email, :created_at, :updated_at,
-        :mail_delay_offset, :send_followup_email, :send_sample_postcard, :completed, :in_progress, :iterations_count, :response_rate, :filter_id, :frequency_per_year, 
+        :mail_delay_offset, :send_followup_email, :send_sample_postcard, :completed, :in_progress, :iterations_count, :response_rate, :filter_id, :frequency_per_year,
         :target_mail_date, :communications_sent
 
       def filter
@@ -16,7 +16,7 @@ module Lobbyist
       def filter=(attributes)
         @filter = Filter.new(attributes)
       end
-      
+
       def self.list(params = {})
         create_collection_from_response(get("/v2/campaigns.json", params))
       end
@@ -31,6 +31,10 @@ module Lobbyist
 
       def self.create(company_id, params)
         create_from_response(post("/v2/campaigns.json", {company_id: company_id, communication_campaign: params}))
+      end
+
+      def self.create_branding_campaign(company_id, params={})
+        create_from_response(post("/v2/campaigns/create_branding_campaign.json", {company_id: company_id, communication_campaign: params}))
       end
 
       def self.update(id, params)
