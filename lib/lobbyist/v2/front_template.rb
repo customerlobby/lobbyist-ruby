f1c28090cb12545633798fa0173a73b1a418d8f6
Sachin S Wagh <sachin.wagh@opcito.com>
2018-06-12 15:58:22 +0530

    CA-7847 #comment Add FrontTemplate.communication_campaigns

--- lib/lobbyist/v2/front_template.rb
+++ lib/lobbyist/v2/front_template.rb
@@ -1,24 +1,26 @@
 module Lobbyist
   module V2
-
     class FrontTemplate < Lobbyist::V2::Base
       attr_accessor :id, :communication_campaign_id, :company_id, :image_filename
       attr_accessor :message_position, :content, :status, :custom_image, :created_at
-      attr_accessor :updated_at, :tags
+      attr_accessor :updated_at, :tags, :communication_campaigns
 
       def self.list(params = {})
         create_collection_from_response(get('/v2/front-templates.json', params))
       end
 
       def self.create(params = {})
-        create_from_response(post("/v2/front-templates.json", {front_template: params}))
+        create_from_response(post('/v2/front-templates.json', front_template: params))
       end
 
       def self.update(id, params = {})
-        create_from_response(put("/v2/front-templates/#{id}.json", {front_template: params}))
+        create_from_response(put("/v2/front-templates/#{id}.json", front_template: params))
       end
 
+      def self.communication_campaigns(id, params = {})
+        response = get("/v2/front-templates/#{id}/communication-campaigns.json", params)
+        create_from_response(response)
+      end
     end
-
   end
 end
