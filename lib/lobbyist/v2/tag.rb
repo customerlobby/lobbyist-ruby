41ad9703985e4667520895f53199f3de6303385d
Sachin S Wagh <sachin.wagh@opcito.com>
2018-07-02 18:32:29 +0530

    CA-8092 #comment Lobbyist : Added attribute in tag.

--- lib/lobbyist/v2/tag.rb
+++ lib/lobbyist/v2/tag.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 module Lobbyist
   module V2
-
+    # Tag class
     class Tag < Lobbyist::V2::Base
-      attr_accessor :id, :name, :display_name
+      attr_accessor :id, :name, :display_name, :is_default
 
       def self.list(params = {})
         create_collection_from_response(get('/v2/tags.json', params))
@@ -13,7 +15,7 @@ module Lobbyist
       end
 
       def self.create(params = {})
-        create_from_response(post("v2/tags", params))
+        create_from_response(post('v2/tags', params))
       end
 
       def self.update(id, params = {})
@@ -23,8 +25,6 @@ module Lobbyist
       def self.destroy(id, params = {})
         create_from_response(delete("/v2/tags/#{id}.json", params))
       end
-
     end
-
   end
 end
