d4b056fe080060f6f74d699674568535f3bad6cc
Tajinder Pal Singh <tsingh@beesolvertechnology.com>
2018-07-04 18:41:50 +0530

    Added Find and Update endpoints for Zipped Uploads

--- lib/lobbyist/v2/zipped_image_upload.rb
+++ lib/lobbyist/v2/zipped_image_upload.rb
@@ -10,6 +10,14 @@ module Lobbyist
       def self.create(params = {})
         create_from_response(post('/v2/zipped_image_uploads.json', { 'zipped_image_upload' => params }, true))
       end
+
+      def self.find(id)
+        create_from_response(get("/v2/zipped_image_uploads/#{id}.json"))
+      end
+
+      def self.update(id, params = {})
+        create_from_response(put("/v2/zipped_image_uploads/#{id}.json", 'zipped_image_upload' => params))
+      end
     end
   end
 end
