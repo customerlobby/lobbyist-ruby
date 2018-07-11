42267ddfc08c4484605a9df89f28159a39d7549b
sreevidya allada <sreevidya.allada@gmail.com>
2018-07-11 05:10:35 -0700

    CA-8027 update custom filter and flter endpoints

--- lib/lobbyist/v2/custom_filter.rb
+++ lib/lobbyist/v2/custom_filter.rb
@@ -1,23 +1,27 @@
 module Lobbyist
   module V2
-    class CustomFilter < Lobbyist::V2::Base
+    class CustomFilter < Lobbyist::V2::HashieBase
       attr_accessor :id, :company_id, :name, :category_name, :tag_name, :description, 
                     :customers_count, :display_name, :status, :created_at, :updated_at
 
       def self.create(company_id, params = {})
-        create_from_response(post("v2/companies/#{company_id}/custom_filters.json", params))
+        create_response(post("v2/companies/#{company_id}/custom_filters.json", params))
       end
 
       def self.destroy(company_id, id)
-        create_from_response(delete("/v2/companies/#{company_id}/custom_filters/#{id}.json"))
+        create_response(delete("/v2/companies/#{company_id}/custom_filters/#{id}.json"))
+      end
+
+      def self.find(company_id, id)
+        create_response(get("/v2/companies/#{company_id}/custom_filters/#{id}.json"))
       end
 
       def self.destroy_tags(company_id, params = {})
-        create_from_response(post("/v2/companies/#{company_id}/custom_filters/destroy-tags.json", params))
+        create_response(post("/v2/companies/#{company_id}/custom_filters/destroy-tags.json", params))
       end
 
       def self.list(company_id, params = {})
-        create_collection_from_response_with_model_name(get('/v2/companies/#{company_id}/custom_filters.json', params))
+        create_response(get("/v2/companies/#{company_id}/custom_filters.json", params))
       end
     end
   end
--- lib/lobbyist/v2/filter.rb
+++ lib/lobbyist/v2/filter.rb
@@ -1,24 +1,24 @@
 module Lobbyist
   module V2
-    class Filter < Lobbyist::V2::Base
+    class Filter < Lobbyist::V2::HashieBase
       attr_accessor :id, :company_id, :name, :short_name, :defined_by, :display_name, :visible
-      attr_accessor :default_filter_id, :feature, :category_name, :description, :customers_count, :created_at, :updated_at
+      attr_accessor :default_filter_id, :feature, :category_name, :description, :customers_count
+      attr_accessor :created_at, :updated_at, :meta_data, :company_meta_data 
 
       def self.create(company_id, params = {})
-        create_from_response(post("v2/companies/#{company_id}/filters.json", params))
+        create_response(post("v2/filters.json", params))
+      end
+
+      def self.find(company_id, id)
+        create_response(get("/v2/filters/#{id}.json"))
       end
 
       def self.destroy(company_id, id)
-        create_from_response(delete("/v2/companies/#{company_id}/filters/#{id}.json"))
+        create_response(delete("/v2/filters/#{id}.json"))
       end
 
-      def self.destroy_tags(company_id, params = {})
-        create_from_response(post("/v2/companies/#{company_id}/filters/destroy-tags.json", params))
-      end
-
-
-      def self.list(params = {})
-        create_collection_from_response_with_model_name(get('/v2/filters.json', params))
+      def self.list(company_id, params = {})
+        create_response(get('/v2/filters.json', params))
       end
     end
   end
