be2b59ee61c368bb22b1842cf1b8ef81eea28649
Osman Afridi <osmanisalso@gmail.com>
2018-07-20 14:57:36 -0700

    CA-8028:Bump version and update changelog.

--- lib/lobbyist/v2/customers_segmentation.rb
+++ lib/lobbyist/v2/customers_segmentation.rb
@@ -1,10 +1,10 @@
 module Lobbyist
   module V2
-    class CustomerSegmentation < Lobbyist::V2::Base
+    class CustomersSegmentation < Lobbyist::V2::Base
       attr_accessor :customers_count, :customers_in_segment, :repeat_customer_rate
 
       def self.list(company_id, params = {})
-        create_collection_from_response(get("/v2/companies/#{company_id}/customers_segmentation.json", params))
+        create_from_response(get("/v2/companies/#{company_id}/customers_segmentation.json", params))
       end
     end
   end
