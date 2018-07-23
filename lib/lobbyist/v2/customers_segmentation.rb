cefd9af30fdd5dec1845d13ac7d420a2eb7b5b23
Osman Afridi <osmanisalso@gmail.com>
2018-07-23 10:26:25 -0700

    CA-8028:Fix endpoint.

--- lib/lobbyist/v2/customers_segmentation.rb
+++ lib/lobbyist/v2/customers_segmentation.rb
@@ -4,7 +4,7 @@ module Lobbyist
       attr_accessor :customers_count, :customers_in_segment, :repeat_customer_rate
 
       def self.list(company_id, params = {})
-        create_from_response(get("/v2/companies/#{company_id}/customers_segmentation.json", params))
+        create_response(get("/v2/companies/#{company_id}/customers_segmentation.json", params))
       end
     end
   end
