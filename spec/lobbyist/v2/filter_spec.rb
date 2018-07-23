e8a40cd3070a83a2ad9b3580555f3c5ece3b685d
Osman Afridi <osmanisalso@gmail.com>
2018-07-23 14:50:00 -0700

    CA-8153:Fix spec and update cassette

--- spec/lobbyist/v2/filter_spec.rb
+++ spec/lobbyist/v2/filter_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 describe Lobbyist::V2::Filter do
-  describe "#list" do
-    it "should return list of filters" do
+  describe '#list' do
+    it 'should return list of filters' do
       VCR.use_cassette('v2/filters_list') do
-        filters = Lobbyist::V2::Filter.list(company_id: 45485)
+        filters = Lobbyist::V2::Filter.list(company_id: 45_485)
 
         expect(filters).to_not be_blank
-        expect(filters.count).to eq(15)
+        expect(filters.items.count).to eq(10)
       end
     end
   end
