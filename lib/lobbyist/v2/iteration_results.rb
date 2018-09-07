1ba5b3d08a7b0e0bd914c461961ecb776807353c
sreevidya allada <sreevidya.allada@gmail.com>
2018-09-07 10:50:16 -0700

    CA-8341 iteration results rubocop changes

--- lib/lobbyist/v2/iteration_results.rb
+++ lib/lobbyist/v2/iteration_results.rb
@@ -1,10 +1,13 @@
+# frozen_string_literal: true
+
 module Lobbyist
   module V2
+    # lobbyist call to make calls to IterationResults resource on api
     class IterationResults < Lobbyist::V2::HashieBase
-      attr_accessor :piece_id, :customer_name, :transacted_at, :amount, :days_since_previous_transaction
+      attr_accessor :piece_id, :customer_name, :transacted_at, :amount, :days_since_prev_txn
 
       def self.list(params = {})
-      	create_response(get('v2/iteration-results.json', params))
+        create_response(get('v2/iteration-results.json', params))
       end
 
       def self.generate_results_file(id, params = {})
