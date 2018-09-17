1d2a87b865c865e0ac63c416ea30048225c802a5
Ramesh Tathe <ramesh.tathe@opcito.com>
2018-09-17 15:55:41 +0530

    Removed commented line

--- lib/lobbyist/error/gateway_timeout.rb
+++ lib/lobbyist/error/gateway_timeout.rb
@@ -1,7 +1,6 @@
 module Lobbyist
   class Error
     # Raised when Customer Lobby returns the HTTP status code 504
-    # Added comment line to test lobbyist gem
     class GatewayTimeout < Lobbyist::Error::ServerError
       HTTP_STATUS_CODE = 504
       MESSAGE = "The Customer Lobby Api servers are up, but the request couldn't be serviced due to some failure within our stack. Try again later."
