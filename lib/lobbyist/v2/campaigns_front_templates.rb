10a30018c3c49ea36a684d722979387560f44747
sreevidya allada <sreevidya.allada@gmail.com>
2019-02-12 04:18:33 -0800

    CA-8876: comment Lobbyist old classes and references: images re-factor

--- lib/lobbyist/v2/campaigns_front_templates.rb
+++ lib/lobbyist/v2/campaigns_front_templates.rb
@@ -1,15 +1,16 @@
-module Lobbyist
-  module V2
-    class CampaignsFrontTemplates < Lobbyist::V2::HashieBase
-      attr_accessor :id, :communication_campaign_id, :company_id, :image_filename
-      attr_accessor :message_position, :content, :custom_image, :created_at, :updated_at
-      attr_accessor :image_resize_status, :category_default_front_template_id, :status
-      attr_accessor :offset
+# Image re-factor (CA--8869)
+# module Lobbyist
+#   module V2
+#     class CampaignsFrontTemplates < Lobbyist::V2::HashieBase
+#       attr_accessor :id, :communication_campaign_id, :company_id, :image_filename
+#       attr_accessor :message_position, :content, :custom_image, :created_at, :updated_at
+#       attr_accessor :image_resize_status, :category_default_front_template_id, :status
+#       attr_accessor :offset
 
-      def self.list(params = {})
-        response = get("/v2/campaigns-front-templates.json", params)
-        create_response(response)
-      end
-    end
-  end
-end
+#       def self.list(params = {})
+#         response = get("/v2/campaigns-front-templates.json", params)
+#         create_response(response)
+#       end
+#     end
+#   end
+# end
--- lib/lobbyist/v2/front_template.rb
+++ lib/lobbyist/v2/front_template.rb
@@ -1,25 +1,26 @@
-module Lobbyist
-  module V2
-    class FrontTemplate < Lobbyist::V2::Base
-      attr_accessor :id, :communication_campaign_id, :company_id, :image_filename
-      attr_accessor :message_position, :content, :status, :custom_image, :created_at
-      attr_accessor :updated_at, :tags, :communication_campaigns, :image_urls
+# Image re-factor (CA--8869)
+# module Lobbyist
+#   module V2
+#     class FrontTemplate < Lobbyist::V2::Base
+#       attr_accessor :id, :communication_campaign_id, :company_id, :image_filename
+#       attr_accessor :message_position, :content, :status, :custom_image, :created_at
+#       attr_accessor :updated_at, :tags, :communication_campaigns, :image_urls
 
-      def self.list(params = {})
-        create_collection_from_response(get('/v2/front-templates.json', params))
-      end
+      # def self.list(params = {})
+      #   create_collection_from_response(get('/v2/front-templates.json', params))
+      # end
 
-      def self.create(params = {})
-        create_from_response(post('/v2/front-templates.json', { front_template: params }, true))
-      end
+      # def self.create(params = {})
+      #   create_from_response(post('/v2/front-templates.json', { front_template: params }, true))
+      # end
 
-      def self.update(id, params = {})
-        create_from_response(put("/v2/front-templates/#{id}.json", front_template: params))
-      end
+      # def self.update(id, params = {})
+      #   create_from_response(put("/v2/front-templates/#{id}.json", front_template: params))
+      # end
 
-      def self.update_campaign_front_template(id, params = {})
-        create_from_response(put("/v2/front-templates/#{id}/update-campaign-front-template.json", params))
-      end
-    end
-  end
-end
+      # def self.update_campaign_front_template(id, params = {})
+      #   create_from_response(put("/v2/front-templates/#{id}/update-campaign-front-template.json", params))
+      # end
+#     end
+#   end
+# end
