45b9d7c35d0da027dabcca67450dbb9163234fbe
sreevidya allada <sreevidya.allada@gmail.com>
2019-04-05 09:00:43 -0700

    CA-8929 : Lobbyist Ruby : destroy end point for front templates

--- lib/lobbyist/version.rb
+++ lib/lobbyist/version.rb
@@ -2,7 +2,7 @@ module Lobbyist
   class Version
     MAJOR = 0 unless defined? Lobbyist::Version::MAJOR
     MINOR = 8 unless defined? Lobbyist::Version::MINOR
-    PATCH = 23 unless defined? Lobbyist::Version::PATCH
+    PATCH = 24 unless defined? Lobbyist::Version::PATCH
 
     class << self
       # @return [String]
