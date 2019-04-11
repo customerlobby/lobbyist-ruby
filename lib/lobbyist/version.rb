65edaaffe8f6aaa565782d4b2838ed8bf282b779
sreevidya allada <sreevidya.allada@gmail.com>
2019-04-11 02:27:20 -0700

    bump the version to 0.8.27

--- lib/lobbyist/version.rb
+++ lib/lobbyist/version.rb
@@ -2,7 +2,7 @@ module Lobbyist
   class Version
     MAJOR = 0 unless defined? Lobbyist::Version::MAJOR
     MINOR = 8 unless defined? Lobbyist::Version::MINOR
-    PATCH = 24 unless defined? Lobbyist::Version::PATCH
+    PATCH = 28 unless defined? Lobbyist::Version::PATCH
 
     class << self
       # @return [String]
