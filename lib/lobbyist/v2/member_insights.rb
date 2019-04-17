08d2622126f264a5df1524b66ff56ca7fe74be7e
Sachin S Wagh <sachin.wagh@opcito.com>
2019-04-17 19:50:15 +0530

    CA-8888: Added HashieBase for MemberInsights

--- lib/lobbyist/v2/member_insights.rb
+++ lib/lobbyist/v2/member_insights.rb
@@ -1,11 +1,11 @@
 module Lobbyist
   module V2
-    class MemberInsights < Lobbyist::V2::Base
+    class MemberInsights < Lobbyist::V2::HashieBase
       attr_accessor :id
 
       def self.insights(company_id, params = {})
         response = get("/v2/member-insights/#{company_id}.json", params)
-        create_from_response(response)
+        create_response(response)
       end
     end
   end
