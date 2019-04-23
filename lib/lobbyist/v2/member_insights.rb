eab59fb99274d96e0fdac40a68d9a075a9b46f09
Sachin S Wagh <sachin.wagh@opcito.com>
2019-04-23 18:03:17 +0530

    CA-8888: Change in Lobbyist::V2::MemberInsights.

--- lib/lobbyist/v2/member_insights.rb
+++ lib/lobbyist/v2/member_insights.rb
@@ -1,9 +1,7 @@
 module Lobbyist
   module V2
     class MemberInsights < Lobbyist::V2::HashieBase
-      attr_accessor :id
-
-      def self.insights(company_id, params = {})
+      def self.find(company_id, params = {})
         response = get("/v2/member-insights/#{company_id}.json", params)
         create_response(response)
       end
