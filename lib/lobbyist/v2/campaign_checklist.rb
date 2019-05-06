e2dcb10961605753c4a12fa55a08f7866cee28b7
sreevidya allada <sreevidya.allada@gmail.com>
2019-05-06 13:01:40 -0700

    CA-9025 : Lobbyist Ruby : replace calculate_readiness for CampaignChecklist with

--- lib/lobbyist/v2/campaign_checklist.rb
+++ lib/lobbyist/v2/campaign_checklist.rb
@@ -4,8 +4,8 @@ module Lobbyist
       attr_accessor :communication_campaign_id, :account_valid, :campaign_valid, :images_count, :credits_short
       attr_accessor :last_data_imported_at, :last_successful_transaction_at, :last_transaction_status, :mail_ready
       
-      def self.calculate_readiness(campaign_id, params = {})
-        create_from_response(get("/v2/campaign-checklist/#{campaign_id}/calculate-readiness.json", params))
+      def self.update(campaign_id, params = {})
+        create_from_response(put("/v2/campaign-checklist/#{campaign_id}.json", params))
       end
     end
   end
