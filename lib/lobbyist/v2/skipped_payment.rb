2040f9c77ee87655997810a13041a1f503267c5f
Usman Asif <usmanasif@users.noreply.github.com>
2019-05-13 03:53:06 -0700

    Added new call to fetch skipped payments for a company

---
+++ lib/lobbyist/v2/skipped_payment.rb
@@ -0,0 +1,14 @@
+module Lobbyist
+  module V2
+
+    class SkippedPayment < Lobbyist::V2::Base
+      attr_accessor :id, :company_id, :admin_user_id, :reason, :source, :transaction_status
+      attr_accessor :amount, :transaction_date, :created_at, :updated_at
+
+      def self.list(company_id, params = {})
+        create_collection_from_response(get("/v2/companies/#{company_id}/company_transactions/skip-payments.json", params))
+      end
+
+    end
+  end
+end
