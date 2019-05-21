7a184184f7875dfc390f49683356de1dc1071544
Usman Asif <usmanasif@users.noreply.github.com>
2019-05-21 00:59:18 -0700

    Updated version because previous is already there

--- lib/lobbyist/version.rb
+++ lib/lobbyist/version.rb
@@ -2,7 +2,7 @@ module Lobbyist
   class Version
     MAJOR = 0 unless defined? Lobbyist::Version::MAJOR
     MINOR = 8 unless defined? Lobbyist::Version::MINOR
-    PATCH = 35 unless defined? Lobbyist::Version::PATCH
+    PATCH = 36 unless defined? Lobbyist::Version::PATCH
 
     class << self
       # @return [String]
