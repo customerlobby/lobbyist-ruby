b8f2a7051edaf1dd831f6cdf04c79b9510f5f2b8
Tajinder Pal Singh <tsingh@beesolvertechnology.com>
2019-07-16 15:30:57 +0530

    Added CreditGrant APIs - [CA-9270]

---
+++ lib/lobbyist/v2/credit_grant.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Lobbyist
+  module V2
+    # Class for getting credit_grants
+    class CreditGrant < Lobbyist::V2::Base
+      attr_accessor :company_id, :cost, :grant_type, :granter_id, :granter_type
+      attr_accessor :status, :expired, :category, :created_at, :updated_at
+      attr_accessor :credit_amount, :total_amount
+
+      def self.summary(company_id)
+        create_collection_from_response(
+            get("/v2/companies/#{company_id}/credit_grants/summary.json")
+        )
+      end
+    end
+
+  end
+end
