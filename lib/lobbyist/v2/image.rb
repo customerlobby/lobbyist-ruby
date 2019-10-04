b2fb4dfd3747a68979d42afe0cd4055b695ab3a7
Tajinder Pal Singh <tsingh@beesolvertechnology.com>
2019-10-04 17:33:11 +0530

    Added API wrapper for image apis - [CA-9580]

---
+++ lib/lobbyist/v2/image.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Lobbyist
+  module V2
+    # class to handle image apis
+    class Image < HashieBase
+      def self.list(params = {})
+        create_response(get('/v2/images.json', params))
+      end
+
+      def self.create(params = {})
+        create_response(post('/v2/images.json', { image: params }, true))
+      end
+    end
+  end
+end
