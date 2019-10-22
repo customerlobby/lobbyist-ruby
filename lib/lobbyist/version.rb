b26c513d3ef6c4ca60f382d231c660f55e198f1c
Ankita <ankita.tandel@opcito.com>
2019-10-22 09:10:33 +0530

    Added changelog

--- lib/lobbyist/version.rb
+++ lib/lobbyist/version.rb
@@ -2,7 +2,7 @@ module Lobbyist
   class Version
     MAJOR = 0 unless defined? Lobbyist::Version::MAJOR
     MINOR = 8 unless defined? Lobbyist::Version::MINOR
-    PATCH = 57.2 unless defined? Lobbyist::Version::PATCH
+    PATCH = 57.3 unless defined? Lobbyist::Version::PATCH
 
     class << self
       # @return [String]
