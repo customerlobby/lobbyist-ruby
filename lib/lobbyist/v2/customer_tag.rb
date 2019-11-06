57fade75cff20142bb03812af973cfd62885c553
sreevs <sreevidya.allada@gmail.com>
2019-11-06 03:58:20 -0800

    CA-9415 small changes

--- lib/lobbyist/v2/customer_tag.rb
+++ lib/lobbyist/v2/customer_tag.rb
@@ -5,7 +5,7 @@ module Lobbyist
       attr_accessor :id, :company_id, :filename, :status, :created_at, :updated_at
 
       def self.create(company_id, params = {})
-        create_from_response(post("/v2/customer_tags.json", { custom_tag: params }, true))
+        create_from_response(post("/v2/customer_tags.json", { customer_tag: params }, true))
       end
     end
   end
