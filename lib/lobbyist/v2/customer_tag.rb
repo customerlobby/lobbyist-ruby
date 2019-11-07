66e767ea6a7683444cfa3075c6bcdecffad9642b
sreevs <sreevidya.allada@gmail.com>
2019-11-07 00:14:39 -0800

    CA-9145 remove company_id from params list

--- lib/lobbyist/v2/customer_tag.rb
+++ lib/lobbyist/v2/customer_tag.rb
@@ -4,7 +4,7 @@ module Lobbyist
     class CustomerTag < Lobbyist::V2::Base
       attr_accessor :id, :company_id, :filename, :status, :created_at, :updated_at
 
-      def self.create(company_id, params = {})
+      def self.create(params = {})
         create_from_response(post("/v2/customer_tags.json", { customer_tag: params }, true))
       end
     end
