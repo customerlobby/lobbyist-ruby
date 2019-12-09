3c012fea695430246ea3723d136fe552831f2775
Ankita <ankita.tandel@opcito.com>
2019-12-09 10:48:26 +0530

    update version

--- lib/lobbyist/version.rb
+++ lib/lobbyist/version.rb
@@ -2,7 +2,7 @@ module Lobbyist
   class Version
     MAJOR = 0 unless defined? Lobbyist::Version::MAJOR
     MINOR = 8 unless defined? Lobbyist::Version::MINOR
-    PATCH = 56 unless defined? Lobbyist::Version::PATCH
+    PATCH = 59.4 unless defined? Lobbyist::Version::PATCH
 
     class << self
       # @return [String]
