76e4ea1c1702983b8a927621ae7bba50845ab87f
sreevs <sreevidya.allada@gmail.com>
2020-02-21 07:24:22 -0800

    CA-10077 more information on promo

--- lib/lobbyist/promo.rb
+++ lib/lobbyist/promo.rb
@@ -3,7 +3,8 @@ module Lobbyist
 
     attr_accessor :promo_id, :account_level_id, :account_class, :promo_code, :promo_description, :is_current,
                   :is_visible, :setup_fee, :monthly_fee, :texting_grants, :first_month_fee, :split_activation_fee, :days_available,
-                  :cost_per_postcard, :customer_count_lower_bound, :customer_count_upper_bound, :tax_exempt
+                  :cost_per_postcard, :customer_count_lower_bound, :customer_count_upper_bound, :tax_exempt,
+                  :text_credits, :postcard_credits, :overage_postcard_cost, :overage_postcard_cost
 
     def self.create(params = {})
       create_from_response(post('/v1/promos.json', { 'promo' => params }))
--- lib/lobbyist/v2/promo.rb
+++ lib/lobbyist/v2/promo.rb
@@ -7,6 +7,7 @@ module Lobbyist
       attr_accessor :promo_description, :is_current, :is_visible, :setup_fee
       attr_accessor :monthly_fee, :texting_grants, :first_month_fee
       attr_accessor :split_activation_fee, :days_available, :cost_per_postcard
+      attr_accessor :text_credits, :postcard_credits, :overage_postcard_cost, :overage_postcard_cost
 
     end
 
