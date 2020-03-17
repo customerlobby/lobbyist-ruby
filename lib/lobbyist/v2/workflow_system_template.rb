230c6ee0934beef447168a48fd772ca7da859518
sreevs <sreevidya.allada@gmail.com>
2020-03-16 19:47:08 -0700

    CA-10301 add supports_diff to V2::WorkflowSystemTemplate

--- lib/lobbyist/v2/workflow_system_template.rb
+++ lib/lobbyist/v2/workflow_system_template.rb
@@ -1,7 +1,7 @@
 module Lobbyist
   module V2
     class WorkflowSystemTemplate < Lobbyist::V2::Base
-      attr_accessor :id, :workflow_system_id, :queries, :days, :time, :upload_type, :importer_class_name
+      attr_accessor :id, :workflow_system_id, :queries, :days, :time, :upload_type, :supports_diff, :importer_class_name
       attr_accessor :fetcher_class_name, :mapper_class_name, :created_at, :updated_at, :split_mappings
 
       def self.list(params = {})
