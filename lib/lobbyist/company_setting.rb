6167629bddd49a8da12c0e848f257026c0ccef12
sreevs <sreevidya.allada@gmail.com>
2020-05-15 04:04:55 -0700

    CA-10406 add review_destinations to company_setting

--- lib/lobbyist/company_setting.rb
+++ lib/lobbyist/company_setting.rb
@@ -6,7 +6,7 @@ module Lobbyist
     :reviews_per_page, :reviews_page_enabled, :request_appointments_enabled, :send_appointment_emails_to, :accept_contact_suggestions,
     :auto_publish_high_score_reviews, :auto_publish_pending_low_score_notification, :auto_email_invitation, :auto_queue_customer_calls, 
     :syndicate_low_score_reviews, :syndicate_reviews, :wants_invoice_emails, :send_invoice_emails_to, :unhappy_customer_feedback, 
-    :review_page_display_element, :created_at, :updated_at
+    :review_page_display_element, :review_destinations, :created_at, :updated_at
 
     def self.update(id, params = {})
       create_from_response(put("/v1/company_settings/#{id}.json", params))
