afc2d77f2b94991646a85a05b2162a0bd1ab1688
Amrinder Singh <amsingh@beesolvertechnology.com>
2021-07-23 12:34:50 +0530

    Initial Commit

---
+++ lib/lobbyist/v2/company_feature_flag.rb
@@ -0,0 +1,17 @@
+module Lobbyist
+  module V2
+    class CompanyFeatureFlag < Lobbyist::V2::Base
+      attr_accessor :id, :component, :operation, :parent_id, :enabled
+
+      class << self
+        def list(company_id, params = {})
+          create_collection_from_response(get("/v2/companies/#{company_id}/company_feature_flags.json", params))
+        end
+
+        def update(company_id, params = {})
+          create_from_response(put("/v2/companies/#{company_id}/company_feature_flags.json", { company_feature_flag: params }))
+        end
+      end
+    end
+  end
+end
