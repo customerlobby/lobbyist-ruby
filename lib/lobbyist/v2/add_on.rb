9cb8a74bbf7b30513ca4c679c940d525c5726a08
cl-nandini <nverma@beesolvertechnology.com>
2021-11-29 10:47:19 +0530

    updated changelog and version

--- lib/lobbyist/v2/add_on.rb
+++ lib/lobbyist/v2/add_on.rb
@@ -4,11 +4,7 @@ module Lobbyist
     class AddOn < Lobbyist::V2::Base
       attr_accessor :id, :parentable_id, :parentable_type, :name, :short_name, :cost, :amount,
                     :status, :rollable, :start_date, :expiration_date
-
-      def self.create(params = {})
-        create_from_response(post('/v1/add_ons.json', { 'add_on' => params }))
-      end
-
+      
     end
 
   end
