5f9038b1ddfa3d43315f517a43ded6dd017edd65
Amrinder Singh <amsingh@beesolvertechnology.com>
2022-08-25 16:25:53 +0530

    Update Company Feature Flag

--- lib/lobbyist/version.rb
+++ lib/lobbyist/version.rb
@@ -2,7 +2,7 @@ module Lobbyist
   class Version
     MAJOR = 0 unless defined? Lobbyist::Version::MAJOR
     MINOR = 8 unless defined? Lobbyist::Version::MINOR
-    PATCH = 79 unless defined? Lobbyist::Version::PATCH
+    PATCH = 80 unless defined? Lobbyist::Version::PATCH
 
     class << self
       # @return [String]
