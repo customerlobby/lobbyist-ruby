2fcda3b13d5bbf9f597114db88046a71dd2087a9
vipin-beesolver <vipink@beesolvertechnology.com>
2023-09-28 15:58:49 +0530

    modified function names

--- lib/lobbyist/v2/app_integration.rb
+++ lib/lobbyist/v2/app_integration.rb
@@ -3,11 +3,11 @@ module Lobbyist
     class AppIntegration < Lobbyist::V2::Base
       attr_accessor :id, :default_app_integration_id, :company_id, :status, :meta_data
 
-      def self.find(company_id, params = {})
+      def self.find_gq_settings(company_id, params = {})
         create_from_response(get("/v2/companies/#{company_id}/gq_settings.json", params))
       end
 
-      def self.update(company_id, params = {})
+      def self.update_gq_settings(company_id, params = {})
         create_from_response(put("/v2/companies/#{company_id}/update_gq_settings.json", params))
       end
 
