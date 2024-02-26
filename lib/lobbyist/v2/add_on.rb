c0428efb0367ec8eae445421aec0d185c34fbbab
cl-nandini <90022636+cl-nandini@users.noreply.github.com>
2024-02-26 13:25:30 +0530

    CA-15403 : Lobbyist : Admin Area Updates to cater for Prospect Marketing (#311)
    * added attributes for prospect marketing
    
    * attrs for prospect marketing
    
    * patch change

--- lib/lobbyist/v2/add_on.rb
+++ lib/lobbyist/v2/add_on.rb
@@ -3,7 +3,8 @@ module Lobbyist
 
     class AddOn < Lobbyist::V2::Base
       attr_accessor :id, :parentable_id, :parentable_type, :name, :short_name, :cost, :amount,
-                    :status, :rollable, :start_date, :expiration_date
+                    :status, :rollable, :start_date, :expiration_date, :prospect_marketing, :unit_price,
+                    :markup_percentage
       
     end
 
