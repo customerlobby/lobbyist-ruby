352461b94bdac31bb4aef0785f22f9636d69c68c
cl-nandini <nverma@beesolvertechnology.com>
2024-04-24 19:22:12 +0530

    gem changes to support tipped on magnet

--- lib/lobbyist/promo.rb
+++ lib/lobbyist/promo.rb
@@ -1,9 +1,12 @@
 module Lobbyist
   class Promo < Lobbyist::Base
 
-    attr_accessor :promo_id, :account_level_id, :account_class, :promo_code, :promo_description, :is_current,
-                  :is_visible, :setup_fee, :monthly_fee, :texting_grants, :first_month_fee, :split_activation_fee, :days_available,
-                  :cost_per_postcard, :customer_count_lower_bound, :customer_count_upper_bound, :tax_exempt, :email_grants, :add_ons, :email_add_ons, :prospect_add_ons
+    attr_accessor :promo_id, :account_level_id, :account_class, :promo_code, :promo_description,
+                  :is_current, :is_visible, :setup_fee, :monthly_fee, :texting_grants,
+                  :first_month_fee, :split_activation_fee, :days_available,
+                  :cost_per_postcard, :customer_count_lower_bound, :customer_count_upper_bound,
+                  :tax_exempt, :email_grants, :add_ons, :email_add_ons, :prospect_add_ons,
+                  :cost_per_magnet_mailer
 
     def self.create(params = {})
       create_from_response(post('/v1/promos.json', { 'promo' => params }))
