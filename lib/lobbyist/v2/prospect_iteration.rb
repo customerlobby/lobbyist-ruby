51339be1559e8a5b7f488cfa3215c01cfddd1ce4
cl-nandini <nverma@beesolvertechnology.com>
2024-05-17 11:33:19 +0530

    changes for prospect iterations

--- lib/lobbyist/v2/prospect_iteration.rb
+++ lib/lobbyist/v2/prospect_iteration.rb
@@ -2,25 +2,13 @@ module Lobbyist
   module V2
 
     class ProspectIteration < Lobbyist::V2::Base
-      attr_accessor :id, :prospect_campaign_id, :start_date, :end_date,
-                    :tracking_start_date, :tracking_end_date
-      attr_accessor :status, :sent_count, :revenue_generated, :amount_spent, :stored_file_status
-      attr_accessor :budget_snapshot, :credit_snapshot, :created_at, :updated_at, :filename, :roi
+      attr_accessor :id, :prospect_campaign_id, :tracking_start_date, :tracking_end_date, :status
+      attr_accessor :sent_count, :converted_contact_count, :call_records, :response_rate, :transaction_count
 
       def self.list(campaign_id, params = {})
-        create_collection_from_response(get("/v2/campaigns/#{campaign_id}/campaign-iterations.json", params))
+        create_collection_from_response(get("/v2/prospect_campaigns/#{campaign_id}/prospect_iterations.json", params))
       end
 
-      def self.generate_results_file(id, params={})
-        response = post("v2/campaign-iterations/#{id}/generate-file.json", params)
-        create_from_response(response)
-      end
     end
-
-    def self.communication_pieces(id, params={})
-      response = get("v2/campaign-iterations/#{id}/communication-pieces.json", params)
-      create_collection_from_response(response)
-    end
-
   end
 end
