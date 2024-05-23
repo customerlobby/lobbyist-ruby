5d3466050a5f5b664f6f7ab940b3de5d6feb5eb9
cl-nandini <nverma@beesolvertechnology.com>
2024-05-23 11:19:03 +0530

    change in prospect Iteration

--- lib/lobbyist/v2/prospect_iteration.rb
+++ lib/lobbyist/v2/prospect_iteration.rb
@@ -3,10 +3,14 @@ module Lobbyist
 
     class ProspectIteration < Lobbyist::V2::Base
       attr_accessor :id, :prospect_campaign_id, :tracking_start_date, :tracking_end_date, :status
-      attr_accessor :sent_count, :converted_contact_count, :call_records, :response_rate, :transaction_count
+      attr_accessor :sent_count, :converted_contact_count, :call_records, :response_rate, :transaction_count, :transactions_referred_count
 
       def self.list(campaign_id, params = {})
         create_collection_from_response(get("/v2/prospect_campaigns/#{campaign_id}/prospect_iterations.json", params))
+      end
+
+      def self.pieces(campaign_id, params = {})
+        create_collection_from_response(get("/v2/prospect_campaigns/#{campaign_id}/prospect_iterations/#{params[:prospect_iteration_id]}/pieces.json", params))
       end
 
     end
