7aa6ea22026f474d4e81dc35c14da37448dc98e7
Amrinder Singh <amsingh@beesolvertechnology.com>
2025-03-05 16:38:31 +0530

    Update Prospect Iteration

--- lib/lobbyist/v2/prospect_iteration.rb
+++ lib/lobbyist/v2/prospect_iteration.rb
@@ -2,8 +2,9 @@ module Lobbyist
   module V2
 
     class ProspectIteration < Lobbyist::V2::Base
-      attr_accessor :id, :prospect_campaign_id, :tracking_start_date, :tracking_end_date, :status
-      attr_accessor :sent_count, :converted_contact_count, :call_records, :response_rate, :transaction_count, :transactions_referred_count, :revenue_total, :revenue_impacted
+      attr_accessor :id, :prospect_campaign_id, :tracking_start_date, :tracking_end_date, :status, :revenue_impacted
+      attr_accessor :sent_count, :converted_contact_count, :call_records, :transactions_referred_count, :revenue_total
+      attr_accessor :opportunity_count, :prospects_count
 
       def self.list(campaign_id, params = {})
         create_collection_from_response(get("/v2/prospect_campaigns/#{campaign_id}/prospect_iterations.json", params))
